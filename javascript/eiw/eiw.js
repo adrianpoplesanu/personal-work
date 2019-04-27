7c93e05f7cdfcc0eb46e3a5f9074248d31634660
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2019-04-28 02:25:55 +0300

    Lexer work

--- javascript/eiw/eiw.js
+++ javascript/eiw/eiw.js
@@ -5,12 +5,29 @@
 console.log('running eiw.js...');
 
 const keywords = {
-    PRINT: 'print',
-    IF: 'if',
-    ELSE: 'else',
-    OPEN_BRAKETS: '{',
-    CLOSE_BRAKETS: '}',
-    INSTRUCTION_END: ';'
+    PRINT: 0,
+    IF: 1,
+    ELSE: 2,
+    OPEN_BRAKETS: 3,
+    CLOSE_BRAKETS: 4,
+    INSTRUCTION_END: 5,
+    VOID_INSTRUCTION: 6,
+    STRING_DEFINITION: 7,
+    NUMBER_DEFINITION: 8,
+    STRING_VALUE: 9,
+    NUMBER_VALUE: 10,
+    UNKNOWN_KEYWORD: 11,
+}
+
+const tokens_to_keywords = {
+    'print': keywords.PRINT,
+    'if': keywords.IF,
+    'else': keywords.ELSE,
+    '{': keywords.OPEN_BRAKETS,
+    '}': keywords.CLOSE_BRAKETS,
+    ';': keywords.INSTRUCTION_END,
+    'string': keywords.STRING_DEFINITION,
+    'number': keywords.NUMBER_DEFINITION,
 }
 
 const identifiers = {
@@ -26,6 +43,11 @@ Parser.prototype.buildAbstractSyntaxTree = function(tokens) {
     var abstractSyntaxTree = new AbstractSyntaxTree();
     abstractSyntaxTree.tokens = tokens;
     return abstractSyntaxTree;
+};
+
+function Token(keyword, value2) {
+    this.keyword = keyword;
+    this.value2 = value2;
 }
 
 function Lexer() {
@@ -34,21 +56,61 @@ function Lexer() {
 
 Lexer.prototype.getLineTokens = function (line) {
     var tokens = [];
-    tokens = line.split(' '); // TODO
+    var pos = 0;
+    var is_string = false;
+    var is_number = false;
+    var token = '';
+    while (pos < line.length) {
+        if (line[pos] == ' ') {
+            if (is_string) {
+                token += ' ';
+            } else {
+                if (token != '') {
+                    tokens.push(new Token(keywords.UNKNOWN_KEYWORD, token));
+                }
+                token = '';
+            }
+        } else if (line[pos] == '"') {
+            if (is_string) {
+                is_string = false;
+                tokens.push(new Token(keywords.STRING_VALUE, token));
+                token = '';
+            } else {
+                is_string = true;
+                //token += line[pos];
+            }
+        } else if (line[pos] == '{') {
+            if (is_string) {
+                token += '{';
+            }
+        } else if (line[pos] == '}') {
+            if (is_string) {
+                token += '}';
+            }
+        } else if (line[pos] == ';') {
+            if (is_string) {
+                token += ';';
+            }
+        } else {
+            token += line[pos];
+        }
+        pos++;
+    }
+    //tokens = line.split(' '); // TODO
     return tokens;
-}
+};
 
 function AbstractSyntaxTree() {
-
+    var astNode = {};
 };
 
 function Action() {
 
 };
 
-Action.prototype.execute = function () {
-    console.log("executing instruction");
-}
+Action.prototype.eval = function () {
+    console.log("evaluating action");
+};
 
 function PrintAction() {
     Action.call(this);
@@ -56,7 +118,7 @@ function PrintAction() {
 
 function IfAction() {
     Action.call(this);
-}
+};
 
 function Program() {
 
@@ -64,7 +126,7 @@ function Program() {
 
 function evaluateExpression() {
 
-}
+};
 
 Program.prototype.traverse = function (source) {
     console.log('traversing...');
