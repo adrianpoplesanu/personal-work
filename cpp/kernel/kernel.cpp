66af52c2537da282832cd1e103121c75f3e87269
Adrian Poplesanu <adrianpoplesanu@ubuntuvm.mshome.net>
2019-06-16 06:19:01 +0300

    Added port.h and port.cpp for comunication with hardware. Made printf prettier. Not pretty enough thou...

--- cpp/kernel/kernel.cpp
+++ cpp/kernel/kernel.cpp
@@ -4,8 +4,32 @@
 void printf(char* str) {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-    for (int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    static uint8_t x = 0;
+    static uint8_t y = 0;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        switch(str[i]) {
+            case '\n':
+                y++;
+                x = 0;
+                break;
+            default:
+                VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+        if (x > 80) {
+            y++;
+            x = 0;
+        }
+        if (y >= 25) {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x <80; x++)
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+            y = 0;
+            x = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();
@@ -17,7 +41,8 @@ extern "C" void callConstructors() {
 }
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/) {
-    printf("running Obstacle1 v0.1...");
+    printf("running Obstacle1 v0.2.4...\n");
+    printf("line 2");
 
     GlobalDescriptorTable gdt;
 
