d66d9486260f7448a21b96a20dced3bf5610c7a1
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-08-18 21:40:26 +0300

    Minot changes

--- cpp/chat-server/server.cpp
+++ cpp/chat-server/server.cpp
@@ -7,7 +7,7 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
-#include <time.h> 
+#include <time.h>
 #include <iostream>
 
 using namespace std;
@@ -15,30 +15,30 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr; 
+    struct sockaddr_in serv_addr;
 
     char sendBuff[1025];
-    time_t ticks; 
+    time_t ticks;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
+    memset(sendBuff, '0', sizeof(sendBuff));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(5000);
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
-    cout<<"aaa";
-    listen(listenfd, 10); 
-    cout<<"bbb";
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    listen(listenfd, 10);
+
     while(1)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        cout<<"someone connected!"<<endl;
 
         ticks = time(NULL);
         snprintf(sendBuff, sizeof(sendBuff), "Adrian's MAC time: %.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff)); 
+        write(connfd, sendBuff, strlen(sendBuff));
 
         close(connfd);
         sleep(1);
