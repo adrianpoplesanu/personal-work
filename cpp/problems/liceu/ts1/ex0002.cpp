7ff1dfbdf84fa540782348ee2a4cb9077ffaf0db
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-01 16:00:14 +0300

    Added a better Makefile and ex0002

---
+++ cpp/problems/liceu/ts1/ex0002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int n, produs = 1;
+    cin >> n;
+    while (n != 0) {
+        int cifra = n % 10;
+        produs *= cifra;
+        n /= 10;
+    }
+    cout << produs << endl;
+    return 0;
+}
