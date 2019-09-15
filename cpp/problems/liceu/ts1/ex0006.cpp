8daf2d45ea1e07b57235849e5f8968d2ee11d7f4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-16 01:23:24 +0300

    TS1 e6 p132

---
+++ cpp/problems/liceu/ts1/ex0006.cpp
@@ -0,0 +1,12 @@
+// TS1 e6 pg132
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    int a, b;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "/ = " << a / b << endl;
+    cout << "% = " << a % b << endl;
+    return 0;
+}
