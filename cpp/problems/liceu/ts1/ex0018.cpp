f1f7278607407f7a25dbf08a576254b12d5737ee
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-21 14:13:18 +0300

    TS1 e2 p162

---
+++ cpp/problems/liceu/ts1/ex0018.cpp
@@ -0,0 +1,11 @@
+// TS1 e2 p162
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "m = " << (float)(a+b)/2 << endl;
+    return 0;
+}
