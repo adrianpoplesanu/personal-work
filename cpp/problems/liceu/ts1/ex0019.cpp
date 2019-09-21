91d12ca9e07e20693a707acc0c03d6b6f26e577b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-21 14:16:47 +0300

    TS1 e3 p162

---
+++ cpp/problems/liceu/ts1/ex0019.cpp
@@ -0,0 +1,12 @@
+// TS1 e3 p162
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b, c;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << a << " " << a + b << " " << a + b + c << endl;
+    return 0;
+}
