d8fbb1b97597bf49ef9004c2805e1df34c28264b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-25 01:19:47 +0300

    TS1 e3 p163

---
+++ cpp/problems/liceu/ts1/ex0022.cpp
@@ -0,0 +1,10 @@
+// TS1 e3 p163
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n;
+    cout << "n = "; cin >> n;
+    if (n % 2 == 0) cout << "e valoare para" << endl;
+    return 0;
+}
