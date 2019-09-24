8a891ba9bcd828327bf5bf8a7d98c481b69d4b4c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-25 01:42:00 +0300

    TS1 e2 p166 v2

---
+++ cpp/problems/liceu/ts1/ex0027.cpp
@@ -0,0 +1,11 @@
+// TS1 e2 p166 v2
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n, inv = 0;
+    cout << "n = "; cin >> n;
+    while (inv*=10, inv+=n%10, n/=10) ;
+    cout << "inv = " << inv << endl;
+    return 0;
+}
