5343ddcd90d1ba893992c67f3e34ee1530258ef6
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-09-25 21:20:15 +0300

    Romb check in two triangles for caesar3-2

---
+++ cpp/problems/liceu/ts1/ex0031.cpp
@@ -0,0 +1,9 @@
+// TS1 e1 p103 pdf
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    for (int i = 5; i > 0; i--) cout << i << " ";
+    cout << endl;
+    return 0;
+}
