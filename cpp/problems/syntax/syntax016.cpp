1b01802d1524f49aab0345d84d2f125f6cb2797c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-10-15 01:21:42 +0300

    syntax fun with reading strings

---
+++ cpp/problems/syntax/syntax016.cpp
@@ -0,0 +1,10 @@
+// char*, char[] and string fun
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string text;
+    cout << "text = "; cin >> text;
+    cout << text << endl;
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax017.cpp
@@ -0,0 +1,11 @@
+// char*, char[] and string fun
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string text;
+    cout << "text = "; 
+    getline(cin, text);
+    cout << text << endl;
+    return 0;
+}
