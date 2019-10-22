3fa200551523bcbd779fb8843ea3af606d868ee3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-10-22 13:36:44 +0300

    Go lang mini server with upload endpoint

---
+++ golang/ramona1/main.go
@@ -0,0 +1,46 @@
+package main
+import(
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("File upload endpoint hit")
+    r.ParseMultipartForm(10 << 20)
+    file, handler, err := r.FormFile("myFile")
+    if err != nil {
+        fmt.Println("Error retrieving file");
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+
+    fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+    fmt.Printf("File Size: %+v\n", handler.Size)
+    fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+    tempFile, err := ioutil.TempFile("temp-images", "upload-*.jpg")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer tempFile.Close()
+
+    fileBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    tempFile.Write(fileBytes)
+    fmt.Fprintf(w, "Successfully uploaded file\n")
+}
+
+func setupRoutes() {
+    http.HandleFunc("/upload", uploadFile)
+    http.ListenAndServe(":9191", nil)
+}
+
+func main() {
+    fmt.Println("running...")
+    setupRoutes()
+}
