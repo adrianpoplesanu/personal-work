ce901ca497480e6733c1126829c45669811ddcf7
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2019-10-25 00:40:32 +0300

    process image and return JSON with points for each face landmark

--- golang/ramona1/analyzer.py
+++ golang/ramona1/analyzer.py
@@ -1,5 +1,26 @@
 import sys
+import face_recognition
+
+def format_points(points):
+    formatted_points = []
+    for point in points:
+        formatted_points.append([point[0], point[1]])
+    return formatted_points
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    print ('{{"filename": "{filename}"}}'.format(filename=filename))
+    image = face_recognition.load_image_file(filename)
+    face_landmarks_list = face_recognition.face_landmarks(image)
+    results = {
+        'filename': filename,
+        'right_eye': face_landmarks_list[0]['right_eye'],
+        'left_eye': face_landmarks_list[0]['left_eye'],
+        'top_lip': face_landmarks_list[0]['top_lip'],
+        'nose_tip': face_landmarks_list[0]['nose_tip'],
+        'left_eyebrow': face_landmarks_list[0]['left_eyebrow'],
+        'bottom_lip': face_landmarks_list[0]['bottom_lip'],
+        'right_eyebrow': face_landmarks_list[0]['right_eyebrow'],
+        'chin': face_landmarks_list[0]['chin'],
+        'nose_bridge': face_landmarks_list[0]['nose_bridge']
+    }
+    print ('{{"filename": "{filename}", "right_eye": {right_eye}}}'.format(filename=results['filename'], right_eye=format_points(results['right_eye'])))
