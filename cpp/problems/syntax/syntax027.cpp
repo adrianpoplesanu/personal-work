96b4dfa4362c09cb5e7f0a5c7a8c777bb427df3f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-11-07 23:50:16 +0200

    hex and variable overflow

---
+++ cpp/problems/syntax/syntax027.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << 0xF << endl;
+    cout << 0xFF << endl;
+    cout << 0xFFF << endl;
+    cout << 0xFFFF << endl;
+    cout << 0x9A << endl;
+    cout << 0x9F << endl;
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax028.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    short int a = 32767;
+    cout << a << endl;
+    a++;
+    cout << a << endl;
+    return 0;
+}
