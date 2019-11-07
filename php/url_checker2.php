786eaf63d65d0aa291ef83be4497bb4ee3339e4e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-11-07 18:16:28 +0200

    updated url_checker2 script

--- php/url_checker2.php
+++ php/url_checker2.php
@@ -2,6 +2,6 @@
     $url = 'http://ec2-52-91-25-4.compute-1.amazonaws.com:9191/';
     $contents = file_get_contents($url);
     if ($contents !== false) {
-        echo $contents;
+        echo $contents . "\n";
     }
 ?>
