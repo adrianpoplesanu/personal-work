b756e725623c6075fe91cfea46b3e1fed2121a34
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-11-16 01:40:46 +0200

    bit move operations

---
+++ cpp/problems/syntax/syntax051.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a = 1;
+    a = a << 1;
+    cout << a << endl;
+    a = a << 1;
+    cout << a << endl;
+    return 0;
+}
