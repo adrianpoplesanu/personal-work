78f0773f44474be10a237b441343333c1c7f592d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-11-29 00:54:37 +0200

    command interpretation and on screen print

--- cpp/problems/big_numbers/main.cpp
+++ cpp/problems/big_numbers/main.cpp
@@ -8,10 +8,26 @@ int main(int argc, char *argv[]) {
     running();
     BigNumber numar1, numar2, numar3;
     numar1.LoadFromString("44748564589467812374783957123");
-    numar2.LoadFromString("10000000000000000000000000000");
+    numar2.LoadFromString("17000000000000000000000000000");
     numar3 = numar1 + numar2;
+    cout << "compiler test: ";
     numar3.PrintNumber();
-    numar1.Add(numar2);
-    numar1.PrintNumber();
+    cout << " ... ok!" << endl;
+    
+    string command;
+    while(1) {
+        cout << "> "; 
+        getline(cin, command);
+        if (command == "help") {
+            cout << "===: Help Big Number v1.0 :===" << endl;
+        }
+        if (command == "version") {
+            cout << "1.0" << endl;
+        }
+        if (command == "exit" || command =="quit") {
+            cout << "Bye!" << endl;
+            break;
+        }
+    }
     return 0;
 }
