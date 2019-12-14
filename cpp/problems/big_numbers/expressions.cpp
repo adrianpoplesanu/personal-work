7f7b5029b3f9bbc901ba820e95614afb9f4d67f2
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-12-14 23:15:23 +0200

    conditionals regexes

--- cpp/problems/big_numbers/expressions.cpp
+++ cpp/problems/big_numbers/expressions.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 bool EvaluateCondition(map<string, BigNumber> variables, string cond) {
-    cout << "evaluating:... " << cond << endl;
+    //cout << "evaluating:... " << cond << endl;
+    regex check_conditional_keywords("\(*(.*)(and|or)(.*)\)*");
+    smatch m;
+    regex_search(cond, m, check_conditional_keywords);
     return true;
 }
