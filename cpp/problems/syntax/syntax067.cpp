2e60e7f23d8c14d7c563948feea0e0261950a660
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2019-12-25 12:25:35 +0200

    aritmetic expression evaluator needs fixes

--- cpp/problems/syntax/syntax067.cpp
+++ cpp/problems/syntax/syntax067.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int precedence (char op) {
-    if(op == '+'||op == '-')
+    if(op == '+' || op == '-')
         return 1;
-    if(op == '*'||op == '/')
+    if(op == '*' || op == '/')
         return 2;
     return 0;
 }
@@ -18,7 +18,7 @@ int applyOp (int a, int b, char op) {
         case '*': return a * b;
         case '/': return a / b;
     }
-    return 0;
+    return 0; // this is dumb
 }
 
 int evaluate (string tokens) {
@@ -27,6 +27,7 @@ int evaluate (string tokens) {
     stack <char> ops;
 
     for (i = 0; i < tokens.length(); i++) {
+        //cout << i << endl;
         if (tokens[i] == ' ')
             continue;
         else if (tokens[i] == '(') {
@@ -36,12 +37,13 @@ int evaluate (string tokens) {
             int val = 0;
             while (i < tokens.length() && isdigit(tokens[i])) {
                 val = (val*10) + (tokens[i]-'0');
-                i++;
+                i++; // asta e problema cu segmentation fault cand paranteza de inchidere nu are spatiu in fata ei
             }
             values.push(val);
         }
 
         else if (tokens[i] == ')') {
+            //cout << "aaa" << endl;
             while (!ops.empty() && ops.top() != '(') {
                 int val2 = values.top();
                 values.pop();
@@ -58,6 +60,7 @@ int evaluate (string tokens) {
             if(!ops.empty())
                ops.pop();
         } else {
+            //cout << "bbb" << endl;
             while (!ops.empty() && precedence(ops.top()) >= precedence(tokens[i])) {
                 int val2 = values.top();
                 values.pop();
@@ -91,10 +94,12 @@ int evaluate (string tokens) {
 }
 
 int main() {
-    cout << evaluate("10 + 2 * 6") << "\n";
-    cout << evaluate("100 * 2 + 12") << "\n";
-    cout << evaluate("100 * ( 2 + 12 )") << "\n";
-    cout << evaluate("100 * ( 2 + 12 ) / 14");
-    cout << endl;
+    //cout << evaluate("10 + 2 * 6 + 1 - 23") << "\n";
+    //cout << evaluate("100 * 2 + 12") << "\n";
+    //cout << evaluate("100 * ( 2 - 12 )") << "\n"; // asta nu merge daca in paranteza pun inca o operatie
+    //cout << evaluate("100 * ( 2 + 12 + 14 ) / 14") << endl;
+    //cout << evaluate("2 * ( 1 + 1 + 2 ) / 2") << endl;
+    //cout << evaluate("1 + 1 + 1 + 1 - 3") << endl;
+    cout << evaluate("2 * ( 1     +1+111 )") << endl;
     return 0;
 }
