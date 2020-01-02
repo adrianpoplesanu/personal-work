5364669c326cf0971fc34d029e10d9a203623d89
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-03 00:05:01 +0200

    some syntax fun

---
+++ objective-c/syntax/syntax004.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+int main() {
+   NSLog(@"Storage size for int : %lu \n", sizeof(int));
+   NSLog(@"Storage size for float : %lu \n", sizeof(float));
+   return 0;
+}
