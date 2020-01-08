fae657656d9c814bf9b8c3bfd552f55f281a377f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-08 12:18:19 +0200

    a simple class test in objective c

---
+++ objective-c/syntax/syntax009.m
@@ -0,0 +1,11 @@
+#include <Foundation/Foundation.h>
+
+@interface SimpleClass : NSObject
+
+@end
+
+int main(int argc, char *argv[]) {
+    printf("running class test...\n");
+    SimpleClass *simpleClass;
+    return 0;
+}
