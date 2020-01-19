0c085ba402c26202b5ddbdd6291f8cc61127c080
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-20 00:16:34 +0200

    more methods added to the nuclear power plant

--- objective-c/syntax/syntax016.m
+++ objective-c/syntax/syntax016.m
@@ -25,10 +25,14 @@
 @end
 
 @interface NuclearPowerPlant : PowerPlant {
-
+@private
+    int megawatts_per_unit;
+    int maximum_allowed_megawatts;
 }
 @property (assign) int total_cores;
 - (void) DisplayNumberOfCores;
+- (void) ChangeMegawattsPerUnit : (int) value withMaximumAllowed : (int) maximum_allowed;
+- (void) DisplayMegawattsOutput;
 @end
 
 @implementation NuclearPowerPlant
@@ -36,8 +40,20 @@
     self = [super init];
     return self;
 }
+- (id) init : (int) total_cores_value {
+    self = [super init];
+    self.total_cores = total_cores_value;
+    return self;
+}
 - (void) DisplayNumberOfCores {
     NSLog(@"%d", _total_cores);
+}
+- (void) ChangeMegawattsPerUnit : (int) value withMaximumAllowed : (int) maximum_allowed {
+    megawatts_per_unit = value;
+    maximum_allowed_megawatts = maximum_allowed;
+}
+- (void) DisplayMegawattsOutput {
+    NSLog(@"%d Mwh [of maximum %d Mwh]", megawatts_per_unit, maximum_allowed_megawatts);
 }
 @end
 
@@ -77,5 +93,9 @@ int main(int argc, char *argv[]) {
     NuclearPowerPlant *nuclearpowerplant = [[NuclearPowerPlant alloc] init];
     nuclearpowerplant.total_cores = 2;
     [nuclearpowerplant DisplayNumberOfCores];
+    [nuclearpowerplant ChangeMegawattsPerUnit : 800 withMaximumAllowed : 3200];
+    [nuclearpowerplant DisplayMegawattsOutput];
+    NuclearPowerPlant *nuclearpowerplant2 = [[NuclearPowerPlant alloc] init : 3];
+    [nuclearpowerplant2 DisplayNumberOfCores];
     return 0;
 }
