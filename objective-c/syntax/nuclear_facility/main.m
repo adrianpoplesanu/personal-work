3d1e9036f052ec26ddcc6e20cc8165e7cffa31f2
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-20 13:05:30 +0200

    main program

---
+++ objective-c/syntax/nuclear_facility/main.m
@@ -0,0 +1,10 @@
+#include <Foundation/Foundation.h>
+#include "powerplant.h"
+
+int main(int argc, char *argv[]) {
+    NSLog(@"running nuclear powerplant simulation...");
+    PowerPlant *powerplant = [[PowerPlant alloc] init];
+    powerplant.name = @"Centrala Nucleara de la Cernavoda";
+    [powerplant LogName];
+    return 0;
+}
