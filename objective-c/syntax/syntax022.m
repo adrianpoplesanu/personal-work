80d0e492174b71eb3260dbe1c2bfec0a24dfbd74
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-22 22:57:05 +0200

    declared an array of objects

---
+++ objective-c/syntax/syntax022.m
@@ -0,0 +1,20 @@
+#include <Foundation/Foundation.h>
+
+@interface City : NSObject {
+
+}
+@end
+
+@implementation City
+
+@end
+
+int main(int argc, char *argv[]) {
+    City *citys[3];
+    //for (int i = 0; i < 3; i++) {
+    //    citys[i] = [[City alloc] init];
+    //}
+    //NSArray *cities = [NSArray arrayWithObjects : citys count : 3];
+    //NSLog(@"%@", cities);
+    return 0;
+}
