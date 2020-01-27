dfa2b0dbd5c7b06ff38629162f1b0a3ebb675bd3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-27 20:48:58 +0200

    initial macos 3d engine basic template

---
+++ cpp/engine3d/macos/Adrian3DEngine1/SampleProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleProgram
+//
+//  Created by Adrian Poplesanu on 1/26/20.
+//  Copyright © 2020 Adrian Poplesanu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Simple sample program!\n";
+    return 0;
+}
