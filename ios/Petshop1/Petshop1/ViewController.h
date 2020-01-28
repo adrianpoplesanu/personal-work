feaf3a15083e8fae20db4fe8db04803dcf5255fd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-01-29 00:06:37 +0200

    Petshop1 app

---
+++ ios/Petshop1/Petshop1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Petshop1
+//
+//  Created by Adrian Poplesanu on 1/28/20.
+//  Copyright © 2020 Adrian Poplesanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/Petshop1/Petshop1UITests/Petshop1UITests.m
@@ -0,0 +1,40 @@
+//
+//  Petshop1UITests.m
+//  Petshop1UITests
+//
+//  Created by Adrian Poplesanu on 1/28/20.
+//  Copyright © 2020 Adrian Poplesanu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Petshop1UITests : XCTestCase
+
+@end
+
+@implementation Petshop1UITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
