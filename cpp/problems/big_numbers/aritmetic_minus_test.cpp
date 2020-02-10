c501ef9cce4b9fa73f1a96cd5e3f05216307a573
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-02-10 11:35:36 +0200

    helper for implementing a minus operation

--- cpp/problems/big_numbers/aritmetic_minus_test.cpp
+++ cpp/problems/big_numbers/aritmetic_minus_test.cpp
@@ -8,7 +8,16 @@ int main(int argc, char *argv[]) {
     BigNumber numar1, numar2, numar3;
     numar1.LoadFromString("6");
     numar2.LoadFromString("5");
-    numar3 = numar1 - numar2;
+    numar1.PrintLineNumber();
+    numar2.PrintLineNumber();
+    //numar3 = numar1 - numar2;
+    if (numar1 > numar2) {
+        cout << "semnul e pozitiv" << endl;
+        numar3 = numar1 - numar2;
+    } else {
+        cout << "semnul e negativ" << endl;
+        numar3 = numar2 - numar1;
+    }
     numar3.PrintLineNumber();
     return 0;
 }
