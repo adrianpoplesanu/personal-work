eb044a4a89e5556718bb0b88681cc6900096070f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-02-13 15:04:09 +0200

    function parameter syntax fun #1

---
+++ objective-c/syntax/syntax033.m
@@ -0,0 +1,14 @@
+#include <Foundation/Foundation.h>
+
+void change_name(NSString *violinist) {
+    violinist = @"Eddy";
+    NSLog(@"%@", violinist);
+}
+
+int main(int argc, char *argv[]) {
+    NSString *violinist = @"Brett";
+    NSLog(@"%@", violinist);
+    change_name(violinist);
+    NSLog(@"%@", violinist);
+    return 0;
+}
