f3514abea466a25eb1223880023db0c792802460
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-02-15 23:56:11 +0200

    update console output

--- javascript/npm_projects/server1/server.js
+++ javascript/npm_projects/server1/server.js
@@ -1,19 +1,15 @@
-// require the http module of node.js
 var http = require('http');
 
-// define the port of access for your server
 const PORT = 8080;
 
-// We need a function which handles requests and send response
-function handleRequest(request, response){
+function handleRequest(request, response) {
+    console.log(request.method + " " + request.headers.host + request.url + " HTTP " + request.httpVersion + "/" + request.httpVersionMajor);
+    console.log("User-Agent: " + request.headers['user-agent']);
     response.end('Server working properly. Requested URL : ' + request.url);
 }
 
-// Create a server
 var myFirstServer = http.createServer(handleRequest);
 
-// Start the server !
-myFirstServer.listen(PORT, function(){
-    // Callback triggered when server is successfully listening. Hurray!
+myFirstServer.listen(PORT, function() {
     console.log("Server listening on: http://localhost:%s", PORT);
 });
