c8e1bd5b825a84b9ce21af20e6907dfc80013030
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-03-29 15:16:49 +0300

    using kernel4 folder for future work

---
+++ cpp/kernel4/kernel.cpp
@@ -0,0 +1,56 @@
+#include "types.h"
+#include "gdt.h"
+#include "interrupts.h"
+
+void printf(char* str) {
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x = 0;
+    static uint8_t y = 0;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        switch(str[i]) {
+            case '\n':
+                y++;
+                x = 0;
+                break;
+            default:
+                VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+        if (x > 80) {
+            y++;
+            x = 0;
+        }
+        if (y >= 25) {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x <80; x++)
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+            y = 0;
+            x = 0;
+        }
+    }
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors() {
+    for (constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i) ();
+}
+
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/) {
+    printf("running Obstacle1 v0.4.8...\n");
+
+    printf("declaring gdt...\n");
+    GlobalDescriptorTable gdt;
+    printf("declaring interupt manager...\n");
+    InterruptManager interrupts(0x20, &gdt);
+    printf("activating interrupts\n");
+    interrupts.Activate();
+    printf("entering OS loop\n");
+
+    while(1);
+}
---
+++ cpp/kernel4/readme.txt
@@ -0,0 +1,5 @@
+Developed and compiled on Ubuntu.
+Dependencies: xorriso, grub
+
+To create the iso run:
+make mykernel.iso
