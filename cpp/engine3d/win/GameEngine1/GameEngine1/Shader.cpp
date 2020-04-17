ca2e05696dbba4b9c21ee46c1c8578bc0831bf13
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-04-17 12:35:41 +0300

    windows opengl 3d game engine

---
+++ cpp/engine3d/win/GameEngine1/GameEngine1/Shader.cpp
@@ -0,0 +1,34 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Shader {
+private:
+	string filename;
+	string source;
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+public:
+	Shader(string fn) {
+		filename = fn;
+		//ifstream in;
+	}
+
+	void LoadShader() {
+		ifstream in(filename);
+		char a[100];
+		in >> a;
+		cout << a << endl;
+	}
+
+	~Shader() {
+
+	}
+};
