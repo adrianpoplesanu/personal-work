652785ed3c497d84a776f5cd64a1a91630106a8f
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-04-22 12:18:56 +0300

    LayoutTesting android app for playing arounf with activities

---
+++ android/LayoutTesting/app/src/main/java/com/adrianpoplesanu/layouttesting/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.adrianpoplesanu.layouttesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("LayoutTesting Debug", "main activity loaded")
+    }
+}
