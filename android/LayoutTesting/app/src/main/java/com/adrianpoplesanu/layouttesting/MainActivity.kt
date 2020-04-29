142d19b1a8c518e2cd5e72f0d083ecf2750f8482
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-04-29 18:51:32 +0300

    dinamically generated textView

--- android/LayoutTesting/app/src/main/java/com/adrianpoplesanu/layouttesting/MainActivity.kt
+++ android/LayoutTesting/app/src/main/java/com/adrianpoplesanu/layouttesting/MainActivity.kt
@@ -3,6 +3,8 @@ package com.adrianpoplesanu.layouttesting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("LayoutTesting Debug", "main activity loaded")
+
+        val text_view_dinamic = TextView(this)
+        text_view_dinamic.textSize = 20f
+        text_view_dinamic.text = "Message received";
+        //text_view_dinamic
+        //ll_main_layout.addView(text_view_dinamic)
+        val linearLayout = findViewById<ConstraintLayout>(R.id.rootLayout)
+        linearLayout?.addView(text_view_dinamic)
     }
 }
