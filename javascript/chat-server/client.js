dfc3e828295013583689d4388d564237e8882fa9
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-05-01 18:01:55 +0300

    updated the server IP

--- javascript/chat-server/client.js
+++ javascript/chat-server/client.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 
-let socket = new WebSocket("ws://localhost:8090");
+//let socket = new WebSocket("ws://localhost:8090");
+let socket = new WebSocket("ws://192.168.0.108:8090");
 
 socket.onopen = function(e) {
     data = {"operation": "send-message", "message": "neata adrian!!!"};
