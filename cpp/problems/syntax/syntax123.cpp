60601f1e4800c388334a0b04422a4770f7015f8c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-05-23 10:05:12 +0300

    testing stdout on pipes

---
+++ cpp/problems/syntax/syntax123.cpp
@@ -0,0 +1,10 @@
+// testing pipes
+// ./syntax123 | grep "es"
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "testing pipes";
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax124.cpp
@@ -0,0 +1,10 @@
+// echo "aaa" | ./syntax124 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s;
+    cin >> s;
+    cout << "Input was: " << s << endl << "done!" << endl;
+    return 0;
+}
