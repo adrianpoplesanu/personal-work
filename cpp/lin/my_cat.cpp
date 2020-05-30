00bf54e30588ff2e7d2b02f108d47526c5cd0a4b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-05-30 10:46:32 +0300

    some cat fun

---
+++ cpp/lin/my_cat.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string text;
+    cin >> text;
+    //getline(text);
+    cout << text << endl;
+    return 0;
+}
