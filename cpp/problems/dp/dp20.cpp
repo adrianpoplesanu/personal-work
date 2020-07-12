b85c3fb04c30d65ff1a59ba18a6fad164d662bc8
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-07-12 12:16:24 +0300

    empty commit

---
+++ cpp/problems/dp/dp20.cpp
@@ -0,0 +1,16 @@
+// http://info.mcip.ro/?cap=Programare%20dinamica ; ex #202
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n;
+    cout << "n = "; cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            
+        }
+    }
+    return 0;
+}
