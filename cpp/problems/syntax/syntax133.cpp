dca7239d1c6204e62745da2fd15fb8c38770fb21
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-11-20 13:23:23 +0200

    bitwise operation test for infoarena problem investigation

---
+++ cpp/problems/syntax/syntax133.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << (4 ^ 2 ^ 6) << "\n";
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax134.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << (6 ^ 6 ^ 6 ^ 15) << "\n";
+    return 0;
+}
