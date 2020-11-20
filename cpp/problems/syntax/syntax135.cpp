4d89608efe9762963dfa50fe8e6533a998e99545
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-11-20 13:25:06 +0200

    number of bytes used by primary integral data types

---
+++ cpp/problems/syntax/syntax135.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    cout << "sizeof(char) = " << sizeof(char) << "\n";
+    cout << "sizeof(int) = " << sizeof(int) << "\n";
+    cout << "sizeof(double) = " << sizeof(double) << "\n";
+    cout << "sizeof(float) = " << sizeof(float) << "\n";
+    return 0;
+}
