e729defb72b074d5bb3a24e5b0bd33f92dc22b5c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-11-26 16:38:13 +0200

    ts2 p13 e1

---
+++ cpp/problems/liceu/ts2/ex014.cpp
@@ -0,0 +1,13 @@
+// ts2 p13 e1
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    float A[7][4][2];
+    float (*p)[4][2];
+
+    long b[9][7][8][5];
+    long (*q)[7][8][5];
+
+    return 0;
+}
