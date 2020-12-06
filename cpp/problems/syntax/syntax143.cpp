8b1a47fa495dc3dd3439432fb58cfcf441f44a7f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-06 19:00:00 +0200

    small code example for Andrei's math homework

---
+++ cpp/problems/syntax/syntax143.cpp
@@ -0,0 +1,9 @@
+// pentru Andrei
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    float i;
+    for (i = 0; i < 8; i++) cout << i / 8 << "\n";
+    return 0;
+}
