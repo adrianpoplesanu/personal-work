a87f4ec5fea4458c1ee815ce14fc686618e0c383
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-11 21:22:41 +0200

    lambda function in c++ - need to update the compiler locally

---
+++ cpp/problems/syntax/c11/syntax007.cpp
@@ -0,0 +1,12 @@
+// this works here: https://repl.it/languages/cpp11
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    auto func1 = [](int i = 6) { return i + 4; };
+    //auto y = [] (int first, int second) {
+    //    return first + second;
+    //};
+    return 0;
+}
