81f5d5b76016a416c41d01e0ce0c3561583b40f9
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-15 02:16:41 +0200

    typescrpit syntax

---
+++ typescript/test1/syntax/syntax001.js
@@ -0,0 +1,8 @@
+var a = 5;
+function test(b) {
+    ++b;
+    console.log(b);
+}
+console.log(a);
+test(a);
+console.log(a);
---
+++ typescript/test1/syntax/syntax001.ts
@@ -0,0 +1,10 @@
+let a = 5;
+
+function test(b) {
+    ++b;
+    console.log(b);
+}
+
+console.log(a);
+test(a);
+console.log(a);
