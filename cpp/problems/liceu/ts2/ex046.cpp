58ddd9c49ed0da499ad7da4e3858ca0597a5c5c6
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-16 01:02:11 +0200

    ts2 p26 e1

---
+++ cpp/problems/liceu/ts2/ex046.cpp
@@ -0,0 +1,13 @@
+// ts2 p26 e1
+#include <iostream>
+using namespace std;
+
+int prod (int x, int y) {
+    return x * y;
+}
+
+int main (int argc, char *argv[]) {
+    int x = 2, y = 3;
+    cout << 1 + prod(x, y) << "\n";
+    return 0;
+}
