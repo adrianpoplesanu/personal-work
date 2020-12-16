7e7bd41fe898bed8c4b873b9a28e443c5bc86c74
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-17 00:38:34 +0200

    ts2 p26 e2

---
+++ cpp/problems/liceu/ts2/ex047.cpp
@@ -0,0 +1,12 @@
+// ts2 p26 e2
+#include <iostream>
+using namespace std;
+
+void test(int a, int b) {
+    cout << a << " " << b << "\b";
+}
+
+int main (int argc, char *argv[]) {
+    test (2 - 3, 2 + 3);
+    return 0;
+}
