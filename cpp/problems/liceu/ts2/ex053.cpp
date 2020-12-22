ba4f697953680de7f8fc30549af021e2da4436e4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2020-12-22 13:25:10 +0200

    ts2 p30 e1

---
+++ cpp/problems/liceu/ts2/ex053.cpp
@@ -0,0 +1,10 @@
+// ts2 p30 e1
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    int n = 4, s = 0;
+    for (int i = 0; i < n; ++i) s += i;
+    cout << s << '\n';
+    return 0;
+}
