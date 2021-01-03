781ca08dc615e4913be3ccecaf5e2efbb65f4493
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-01-03 16:00:19 +0200

    distributed initial commit

---
+++ cpp/repl/distributed/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "program.h"
+#include "program.cpp"
+#include "parser.h"
+#include "parser.cpp"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Running Distributed 0.1\n";
+    return 0;
+}
---
+++ cpp/repl/distributed/program.h
@@ -0,0 +1,11 @@
+#ifndef __PROGRAM_H
+#define __PROGRAM_H
+
+class Program {
+public:
+    Program();
+    ~Program();
+    void Run();
+};
+
+#endif 
