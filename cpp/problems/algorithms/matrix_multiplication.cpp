f7446a71d5325571efece4e52f8391061bf5b432
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-01-05 00:49:25 +0200

    simple algorithm implementation

--- cpp/problems/algorithms/matrix_multiplication.cpp
+++ cpp/problems/algorithms/matrix_multiplication.cpp
@@ -9,22 +9,25 @@ int main(int argc, char *argv[]) {
         {1, 2, 3}
     };
     int B[3][2] = {
-        {1, 1, 2},
-        {1, 2, 2},
-        {3, 3, 1}
+        {1, 1},
+        {1, 2},
+        {3, 3}
     };
 
+    int C[4][2];
+
     for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << A[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
-            cout << B[i][j] << " ";
+            int cell_value = 0;
+            for (int k = 0; k < 3; k++) cell_value += A[i][k] * B[k][j];
+            C[i][j] = cell_value;
         }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) cout << C[i][j] << " ";
         cout << "\n";
     }
+
     return 0;
 }
