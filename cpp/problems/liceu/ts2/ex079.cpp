0d2020a423639b5fff68052e0c3f9a2103593399
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-01-07 00:33:03 +0200

    ts2 p46 e3

---
+++ cpp/problems/liceu/ts2/ex079.cpp
@@ -0,0 +1,14 @@
+// ts2 p46 e3
+#include <iostream>
+using namespace std;
+
+int aria(int n) {
+    return n * n;
+}
+
+int main(int argc, char *argv[]) {
+    int n;
+    cout << "n = "; cin >> n;
+    cout << aria(n) << "\n";
+    return 0;
+}
