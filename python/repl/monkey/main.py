004ba5bb1366191fc6a77786fb35e818afb34003
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-01-29 00:51:47 +0200

    run the interpretor on files also and better looping for cli interpreter

--- python/repl/monkey/main.py
+++ python/repl/monkey/main.py
@@ -1,6 +1,11 @@
+import sys
 from repl import Repl
 
 if __name__ == '__main__':
     print 'welcome to mokey'
-    r = Repl()
-    r.loop()
+    if len(sys.argv) > 1:
+        r = Repl(sys.argv[1])
+        r.loop()
+    else:
+        r = Repl()
+        r.loop()
--- python/repl/monkey/repl.py
+++ python/repl/monkey/repl.py
@@ -3,23 +3,33 @@ from lexer import TokenType
 
 
 class Repl(object):
-    def __init__(self):
+    def __init__(self, filename=None):
         self.lexer = Lexer()
+        self.filename = filename
         #self.lexer = None
 
     def loop(self):
-        while True:
-            line = raw_input('>> ')
-            #self.lexer = Lexer(line)
-            self.lexer.New(line)
+        if self.filename:
+            data = open(self.filename, 'r').read()
+            print data
+            self.lexer.New(data)
             tok = self.lexer.nextToken()
             while tok.token_type != TokenType.EOF:
                 print tok
                 tok = self.lexer.nextToken()
-            #lexing = True
-            #while lexing:
-            #    tok = self.lexer.nextToken(line[0])
-            #    print tok
-            #    if tok.token_type == TokenType.EOF:
-            #        lexing = False
-            #print self.lexer.nextToken(line[0])
+        else:
+            while True:
+                line = raw_input('>> ')
+                #self.lexer = Lexer(line)
+                self.lexer.New(line)
+                tok = self.lexer.nextToken()
+                while tok.token_type != TokenType.EOF:
+                    print tok
+                    tok = self.lexer.nextToken()
+                #lexing = True
+                #while lexing:
+                #    tok = self.lexer.nextToken(line[0])
+                #    print tok
+                #    if tok.token_type == TokenType.EOF:
+                #        lexing = False
+                #print self.lexer.nextToken(line[0])
