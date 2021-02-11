8f40b6c6e1f2a94248d6ec2a1dc081288094eed7
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-11 20:48:57 +0200

    example of input from file

---
+++ cpp/problems/syntax/syntax162.cpp
@@ -0,0 +1,11 @@
+// usage ./syntax162 < README.md
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s;
+    cin >> s;
+    cout << "output: " << s << '\n';
+    cin >> s;
+    return 0;
+}
