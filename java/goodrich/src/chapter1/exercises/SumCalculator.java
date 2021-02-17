e65c57ac479efe39f549eb19bdd5340d1229606e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-17 10:52:35 +0200

    added R1-5

--- java/goodrich/src/chapter1/exercises/SumCalculator.java
+++ java/goodrich/src/chapter1/exercises/SumCalculator.java
@@ -8,10 +8,19 @@ public class SumCalculator {
 		return total;
 	}
 	
+	public int oddSum(int n) {
+		int total = 0;
+		for (int i = 0; i <= n; ++i) if (i % 2 == 0) total += i;
+		return total;
+	}
+	
 	public static void main(String[] args) {
 		SumCalculator sumCalculator = new SumCalculator();
 		System.out.println(sumCalculator.sum(5));
 		System.out.println(sumCalculator.sum(12));
 		System.out.println(sumCalculator.sum(45));
+		System.out.println(sumCalculator.oddSum(5));
+		System.out.println(sumCalculator.oddSum(12));
+		System.out.println(sumCalculator.oddSum(45));
 	}
 }
