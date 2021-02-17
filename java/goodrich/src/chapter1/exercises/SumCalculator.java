58579efaaebfd282fb20ae5bed947081674ba415
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-17 10:56:12 +0200

    implemented R1-6

--- java/goodrich/src/chapter1/exercises/SumCalculator.java
+++ java/goodrich/src/chapter1/exercises/SumCalculator.java
@@ -14,6 +14,12 @@ public class SumCalculator {
 		return total;
 	}
 	
+	public int squareSum(int n) {
+		int total = 0;
+		for (int i = 0; i <= n; ++i) total += i * i;
+		return total;
+	}
+	
 	public static void main(String[] args) {
 		SumCalculator sumCalculator = new SumCalculator();
 		System.out.println(sumCalculator.sum(5));
@@ -22,5 +28,8 @@ public class SumCalculator {
 		System.out.println(sumCalculator.evenSum(5));
 		System.out.println(sumCalculator.evenSum(12));
 		System.out.println(sumCalculator.evenSum(45));
+		System.out.println(sumCalculator.squareSum(5));
+		System.out.println(sumCalculator.squareSum(12));
+		System.out.println(sumCalculator.squareSum(45));
 	}
 }
