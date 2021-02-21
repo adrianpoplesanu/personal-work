a145563bf064287e8ba79df872817a359562b6aa
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-22 00:06:05 +0200

    ctl+c signal handling

--- python/repl/monkey/repl.py
+++ python/repl/monkey/repl.py
@@ -1,5 +1,12 @@
+import signal
+import sys
 from lexer import Lexer
 from lexer import TokenType
+
+
+def signal_ctrl_c_handler(sig, frame):
+    print "\nleving repl, bye bye!"
+    sys.exit(0)
 
 
 class Repl(object):
@@ -8,6 +15,7 @@ class Repl(object):
         self.filename = filename
 
     def loop(self):
+        signal.signal(signal.SIGINT, signal_ctrl_c_handler)
         if self.filename:
             data = open(self.filename, 'r').read()
             print data
