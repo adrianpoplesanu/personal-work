e210c2738f2b09892335a4bec7cad4682bf90454
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-22 00:05:13 +0200

    minimum algorith parallelized

--- python/parallel/minimum_algorithm/main.py
+++ python/parallel/minimum_algorithm/main.py
@@ -2,23 +2,31 @@ import threading
 
 
 class Task(threading.Thread):
-    def __init__(self, id):
+    def __init__(self, id, numbers):
         threading.Thread.__init__(self)
         self.id = id
+        self.numbers = numbers
+        self.result = None
 
     def run(self):
-        print 'starting ' + str(self.id)
+        min = numbers[0]
+        for number in self.numbers:
+            if min > number:
+                min = number
+        self.result = min
 
-def solve(a, num_threads):
+def solve(numbers, num_threads):
     threads = []
+    rang = [(0, 7), (7, 14)]
     for i in range(num_threads):
-        threads.append(Task(i))
+        threads.append(Task(i, numbers[rang[i][0]:rang[i][1]]))
         threads[i].start()
 
     for t in threads:
         t.join()
         print 'finished ' + str(t.id)
+        print t.result
 
 if __name__ == '__main__':
-    a = [67, 45, 32, 87, 34, 12, 6, 3, -5, 67, 4, 78, 89, 2]
-    solve(a, 2)
+    numbers = [67, 45, 32, 87, 34, 12, 6, 3, -5, 67, 4, 78, 89, 2]
+    solve(numbers, 2)
