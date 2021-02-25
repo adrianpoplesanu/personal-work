9c7a976006ced48b12a6701576d290033a0c635a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-25 08:11:39 +0200

    another way of generating random numbers

---
+++ python/utils/random2.py
@@ -0,0 +1,8 @@
+import uuid
+
+if __name__ == '__main__':
+    numbers = str(uuid.uuid4()).split('-')
+    sum = 0
+    for number in numbers:
+        sum += int(number, 16)
+    print str(sum)[-5:]
