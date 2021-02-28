89f2be6a67486cfab99bb136b8faa716f6072302
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-02-28 10:59:06 +0200

    added new expressions to ast

--- python/repl/basic/ast.py
+++ python/repl/basic/ast.py
@@ -1,5 +1,30 @@
-class Statement(object):
+class Node(object):
     pass
 
-class Expression(object):
+class Statement(Node):
+    pass
+
+class Expression(Node):
+    pass
+
+class Program(object):
+    def __init__(self, statements=None):
+        if statements:
+            self.statements = statements
+        else:
+            self.statements = []
+
+class LetStatement(Statement):
+    pass
+
+class ReturnStatement(Statement):
+    pass
+
+class ExpressionStatement(Statement):
+    pass
+
+class IfExpression(Expression):
+    pass
+
+class ForExpression(Expression):
     pass
