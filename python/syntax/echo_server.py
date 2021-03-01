8bd6d5accc08c616c17e7b54c19f85e07175c88b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-01 23:03:16 +0200

    made echo_server accept forever - single thread

--- python/syntax/echo_server.py
+++ python/syntax/echo_server.py
@@ -5,10 +5,11 @@ PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
 while True:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+    conn.close()
