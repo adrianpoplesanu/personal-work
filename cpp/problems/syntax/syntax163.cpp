25c6e0e5f21dff17160ab786c9019750ce8d9251
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-04 12:00:31 +0200

    usage of unix strings command on binaries

---
+++ cpp/problems/syntax/syntax163.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "buna dimineata!\n";
+    for (int i = 0; i < 4; i++) {
+        cout << "neatza!\n";
+    }
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax163.sh
@@ -0,0 +1,6 @@
+make clean syntax163 ;
+echo 'original output'
+./syntax163 ;
+echo 'strings of binary'
+strings syntax163 ;
+make clean
