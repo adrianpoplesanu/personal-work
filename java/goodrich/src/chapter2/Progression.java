baf4f0900e57d1965e86aaf5a3125417dec709b0
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-05 21:07:01 +0200

    simple progression class

---
+++ java/goodrich/src/chapter2/Progression.java
@@ -0,0 +1,30 @@
+package chapter2;
+
+public class Progression {
+	protected long current;
+	
+	public Progression() {
+		this(0);
+	}
+	
+	public Progression(long start) {
+		current = start;
+	}
+	
+	public long nextValue() {
+		long answer = current;
+		advance();
+		return answer;
+	}
+	
+	protected void advance() {
+		current++;
+	}
+	
+	public void printProgression(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(" " + nextValue());
+		}
+		System.out.println();
+	}
+}
