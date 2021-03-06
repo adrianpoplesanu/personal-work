98d936deec48a3dc3090374f2ba5cc297072b06b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-06 06:54:57 +0200

    syntaxt explanation

--- python/syntax/quine.py
+++ python/syntax/quine.py
@@ -11,3 +11,33 @@ print '==='
 
 a='a=%r'
 print a % a
+
+print '==='
+
+b = 'ccc%s'
+print b % b
+
+print '==='
+
+c = 'ccc%r'
+print c % c
+
+print '==='
+d = 'aa %%s bb'
+#print d % 'c'
+print d % ()
+
+print '==='
+print "%%" % ()
+
+print '==='
+print '%% %s' % 'a'
+
+print '==='
+print '%%'
+print '%%' % ()
+
+print '==='
+# asta e motivul pentru care merge, 'aa' e pus in primul %s iar %% e evaluat catre % cand e formatat
+# stringul, pentru ca asta e modul in care afisezi % in formatare
+print '%s %%' % 'aa'
