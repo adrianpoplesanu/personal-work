98ecfd2a830cecac0343d00772e8c4501428625e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-06 17:28:35 +0200

    current_token_is, peek_token_is and parse_statement

--- python/repl/basic/parser.py
+++ python/repl/basic/parser.py
@@ -17,7 +17,16 @@ class Parser(object):
         self.peek_token = self.lexer.next_token()
 
     def current_token_is(self, token_type):
-        return 
+        return self.current_token.token_type == token_type
+
+    def peek_token_is(self, token_type):
+        return self.peek_token.token_type == token_type
+
+    def expect_token(self, token_type):
+        if self.peek_token_is(token_type):
+            self.next_token()
+            return True
+        return False
 
     def parse_program(self):
         # this returns a program based on the source of the lexer
@@ -27,15 +36,23 @@ class Parser(object):
     def build_program_statements(self, program):
         # this returns a program based on the source of the lexer, this is better naming for this method
         while self.current_token.token_type != TokenType.EOF:
-            statement = self.current_token
-            program.statements.append(statement)
+            statement = self.parse_statement()
+            if statement:
+                program.statements.append(statement)
             self.next_token()
 
     def parse_statement(self):
-        pass
+        if self.current_token.token_type == TokenType.LET:
+            self.parse_let_statement()
+        if self.current_token.token_type == TokenType.RETURN:
+            self.parse_return_statement()
+        return self.current_token
 
     def parse_let_statement(self):
         pass
 
     def parse_return_statement(self):
+        pass
+
+    def parse_expression_statement(self):
         pass
