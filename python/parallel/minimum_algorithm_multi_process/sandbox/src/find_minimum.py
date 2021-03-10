740cc80435b304adb42e7acae0c26a2eca0e21c0
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-11 00:53:04 +0200

    multi process find minimum algorithm

--- python/parallel/minimum_algorithm_multi_process/sandbox/src/find_minimum.py
+++ python/parallel/minimum_algorithm_multi_process/sandbox/src/find_minimum.py
@@ -1,6 +1,8 @@
+import time
 from multiprocessing import Process, Queue
 
 def worker(q, items):
+    time.sleep(5)
     min = items[0]
     for item in items:
         if min > item:
@@ -8,10 +10,15 @@ def worker(q, items):
     q.put(min)
 
 if __name__ == '__main__':
+    processes = []
     q = Queue()
     for i in range(5):
         p = Process(target=worker, args=(q, [4, 3, 1, 6, 3, -2, 9],))
+        processes.append(p)
         p.start()
+
+    for p in processes:
         p.join()
+
     while not q.empty():
         print q.get()
