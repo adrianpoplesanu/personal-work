ec9d6f3072ba5355217d7e9dad33ea4d6389feda
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-12 14:11:54 +0200

    a simple numbers generator script

---
+++ golang/parralel/find_minimum/README.md
@@ -0,0 +1,3 @@
+How to generate some numbers:
+
+./numbers_generator.py -o numbers.txt -s 100
---
+++ golang/parralel/find_minimum/numbers_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import argparse
+import random
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", help="output file with random numbers")
+    parser.add_argument("-s", "--size", help="number of random entries in the file")
+    args = parser.parse_args()
+
+    filename = args.output
+    size = args.size
+
+    with open(filename, 'w') as f:
+        for i in range(int(size)):
+            f.write(str(random.randint(0, 9)) + ' ')
