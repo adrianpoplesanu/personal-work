75cd64bff2148f960fbba10df16f8251f0b435b6
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-03-15 00:47:36 +0200

    started work on Lexer

--- cpp/repl/monkey/lexer.cpp
+++ cpp/repl/monkey/lexer.cpp
@@ -1,9 +1,40 @@
 #include "lexer.h"
+#include <string>
+using namespace std;
 
 Lexer::Lexer() {
-
+    position = 0;
+    readPosition = 0;
 }
 
 Lexer::~Lexer() {
-    
+
+}
+
+void Lexer::Load(string s) {
+    position = 0;
+    readPosition = 0;
+    source = s;
+    ReadChar();
+}
+
+void Lexer::ReadChar() {
+    if (readPosition < source.length()) {
+        current_char = source[readPosition];
+    } else {
+        current_char = 0;
+    }
+    position = readPosition;
+    readPosition++;
+}
+
+char Lexer::NextToken() {
+    switch(current_char) {
+        case 0:
+            return 0;
+            break;
+        default:
+            return current_char;
+            break;
+    }
 }
--- cpp/repl/monkey/lexer.h
+++ cpp/repl/monkey/lexer.h
@@ -1,10 +1,22 @@
 #ifndef __LEXER_H
 #define __LEXER_H
 
+#include <string>
+using namespace std;
+
 class Lexer {
 public:
     Lexer();
     ~Lexer();
+    void Load(string);
+    void ReadChar();
+    char NextToken();
+
+private:
+    string source;
+    int position;
+    int readPosition;
+    char current_char;
 };
 
 #endif
