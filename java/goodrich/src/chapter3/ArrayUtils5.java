6c885dd5e17862b87ef578976afeb0a395e93f91
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-04-04 00:23:22 +0300

    added copyOf and retrieved it from Array.class as copyOf2 and implemented it in with my own copyOf version

--- java/goodrich/src/chapter3/ArrayUtils5.java
+++ java/goodrich/src/chapter3/ArrayUtils5.java
@@ -1,5 +1,7 @@
 package chapter3;
 
+import java.lang.reflect.Array;
+
 public class ArrayUtils5 {
 	public static <T> void fill(T[] a, T val) {
 		for (int i = 0; i < a.length; i++) {
@@ -19,10 +21,34 @@ public class ArrayUtils5 {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static <T> T[] copyOf(T[] a, int n) {
-		// TODO: implement it
-		return null;
+		//T b[] = (T[]) new Object[n];
+		T b[] = (T[]) Array.newInstance(a.getClass().getComponentType(), n);
+		for (int i = 0; i < n; i++) {
+			if (i < a.length) b[i] = a[i];
+			else b[i] = null;
+		}
+		return b;
 	}
+	
+    @SuppressWarnings("unchecked")
+    public static <T> T[] copyOf2(T[] original, int newLength) {
+        return (T[]) copyOf2(original, newLength, original.getClass());
+    }
+	
+    @SuppressWarnings("unchecked")
+	public static <T,U> T[] copyOf2(U[] original, int newLength, Class<? extends T[]> newType) {
+        T[] copy = ((Object)newType == (Object)Object[].class)
+            ? (T[]) new Object[newLength]
+            : (T[]) Array.newInstance(newType.getComponentType(), newLength);
+        if (newLength > original.length) {
+        	for(int i = 0; i < original.length; i++) copy[i] = (T) original[i];
+        } else {
+        	for(int i = 0; i < newLength; i++) copy[i] = (T) original[i];
+        }
+        return copy;
+    }
 	
 	public static int[] copyOf(int[] a, int n) {
 		// TODO: implement it
@@ -36,5 +62,20 @@ public class ArrayUtils5 {
 		boolean b[] = new boolean[5];
 		ArrayUtils5.fill(b, true);
 		for (int i = 0; i < b.length; i++) System.out.println(b[i]);
+		String c[] = new String[10]; // this is so stupid java, java!
+		c = ArrayUtils5.copyOf(a, 10);
+		for (int i = 0; i < c.length; i++) System.out.println(c[i]);
+		Os os[] = new Os[5];
+		Os base = new Os();
+		base.setName("lin");
+		ArrayUtils5.fill(os, base);
+		for (int i = 0; i < os.length; i++) System.out.println(os[i].getName());
+		Os os2[]; // this is so stupid, java!
+		//System.out.println(ArrayUtils5.copyOf(os, 10).getClass().toString());
+		os2 = ArrayUtils5.copyOf2(os, 10);
+		System.out.println(os2.length);
+		for (int i = 0; i < os2.length; i++) {
+			System.out.println(os2[i]);
+		}
 	}
 }
