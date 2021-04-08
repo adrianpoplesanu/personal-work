7fc0f09703ae958bf37eb15d083b2a1687d0c424
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-04-09 01:29:02 +0300

    integer and boolean objects

--- python/repl/monkey/object.py
+++ python/repl/monkey/object.py
@@ -1,9 +1,33 @@
 class Object(object):
-    pass
-
-class ObjectType(object):
     def Type(self):
         pass
 
     def Inspect(self):
         pass
+
+
+class ObjectType(object):
+    INTEGER_OBJ = "INTEGER"
+    BOOLEAN_OBJ = "BOOLEAN"
+
+
+class Integer(object):
+    def __init__(self):
+        self.Value = '47'
+
+    def Inspect(self):
+        print "{0}".format(self.Value)
+
+    def Type(self):
+        return ObjectType.INTEGER_OBJ
+
+
+class Boollean(object):
+    def __init__(self):
+        self.Value = 'false'
+
+    def Inspect(self):
+        print "{0}".format(self.Value)
+
+    def Type(self):
+        return ObjectType.BOOLEAN_OBJ
