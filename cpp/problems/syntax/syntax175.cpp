ab2aab89da5a955b19f21626f36ab00f964985b3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-04-09 16:10:45 +0300

    cool implementation of a single linked list with a generic type

---
+++ cpp/problems/syntax/syntax175.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+struct Node {
+    T* info;
+    Node<T>* next;
+};
+
+template<typename T>
+class SingleLinkedList {
+private:
+    Node<T>* head;
+    Node<T>* tail;
+    int size;
+public:
+    SingleLinkedList() {
+        size = 0;
+    }
+    ~SingleLinkedList() {
+	while(head) {
+            Node<T>* next = head->next;
+            delete head->info;
+            delete head;
+            head = next;
+        }
+    }
+    void Append(T elem) {
+        if (head) {
+            Node<T>* newest = new Node<T>();
+            tail->next = newest;
+            tail = newest;
+            size++;
+            tail->info = new T();
+            *(tail->info) = elem;
+        } else {
+            head = new Node<T>();
+            tail = head;
+            size++;
+            head->info = new T();
+            *(head->info) = elem;
+        }
+    }
+    void Println() {
+        Node<T>* c = head;
+        cout << "[";
+        while(c) {
+            cout << *c->info;
+            c = c->next;
+            if (c) cout << ", ";
+        }
+        cout << "]";
+        cout << "\n";
+    }
+};
+
+int main(int argc, char *argv[]) {
+    SingleLinkedList<int> lista;
+    lista.Append(5);
+    lista.Append(7);
+    lista.Println();
+
+    SingleLinkedList<string> users;
+    users.Append("Adrianus");
+    users.Append("Dexiciul");
+    users.Append("Ramonice");
+    users.Println();
+    return 0;
+}
