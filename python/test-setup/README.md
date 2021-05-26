a2ff32cd08f6cee55e65175733210a2800e7e233
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-05-26 13:40:58 +0300

    relpicating the issue and adding the fix

---
+++ python/test-setup/README.md
@@ -0,0 +1,10 @@
+virtualenv sandbox
+
+(sandbox) 192-168-0-103:test-setup adrianpoplesanu$ python
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 05:52:31) 
+[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+
+(sandbox) 192-168-0-103:test-setup adrianpoplesanu$ pip --version
+pip 21.1.2 from /Users/adrianpoplesanu/Documents/git-projects/personal-work/python/test-setup/sandbox/lib/python3.6/site-packages/pip (python 3.6)
--- python/test-setup/test_setup.py
+++ python/test-setup/test_setup.py
@@ -1,10 +1,15 @@
-#from pip._internal.req import parse_requirements
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
+#from pip.req import parse_requirements
 
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 reqs = parse_requirements(path.join(here, 'requirements.in'), session='fake')
-requirements = [str(req.req) for req in reqs]
+# reqs is a generator, i need to persist it in a list
+install_reqs = list(reqs)
+try:
+    requirements = [str(req.req) for req in install_reqs]
+except AttributeError:
+    requirements = [str(req.requirement) for req in install_reqs]
 
 print (requirements)
