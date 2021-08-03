19062d07b9b5fac15cd138178354ab5026e547a4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-08-04 00:15:19 +0300

    implemented a single linked list for usage in a stack

---
+++ cpp/problems/algorithms/single_linked_list2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+template<typename T>
+struct Node {
+    T data;
+    Node<T>* next;
+};
+
+template<typename T>
+class SingleLinkedList {
+private:
+    Node<T>* head;
+    Node<T>* tail;
+    int size;
+public:
+    SingleLinkedList() {
+        size = 0;
+    }
+
+    ~SingleLinkedList() {
+        Node<T>* current;
+        while(head) {
+            current = head;
+            head = head->next;
+            delete current;
+        }
+    }
+
+    bool IsEmpty() {
+        return size == 0;
+    }
+
+    int Size() {
+        return size;
+    }
+
+    void AddFirst(T elem) {
+        Node<T>* obj = new Node<T>();
+        obj->data = elem;
+        size++;
+        if (head == NULL) {
+            head = obj;
+            tail = obj;
+        } else {
+            obj->next = head;
+            head = obj;
+        }
+    }
+
+    void AddLast(T elem) {
+        Node<T>* obj = new Node<T>();
+        obj->data = elem;
+        size++;
+        if (tail) {
+            tail->next = obj;
+            tail = obj;
+        } else {
+            head = obj;
+            tail = obj;
+        }
+    }
+
+    T first() {
+        return head;
+    }
+
+    T last() {
+        return tail;
+    }
+
+    void Print() {
+        Node<T>* current = head;
+        while(current) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << "\n";
+    }
+};
