ccd6eb59d0c88045d74feacbf33605cc3c31883e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-08-06 23:01:08 +0300

    html validator

--- java/goodrich/src/chapter6/StackUtils.java
+++ java/goodrich/src/chapter6/StackUtils.java
@@ -41,6 +41,26 @@ public class StackUtils {
 		return stack.isEmpty();
 	}
 
+	public static boolean isHTMLValid(String html) {
+		Stack<String> buffer = new LinkedListStack<String>();
+		int j = html.indexOf('<');
+		while (j != -1) {
+			int k = html.indexOf('>', j + 1);
+			if (k == -1) return false;
+			String tag = html.substring(j + 1, k);
+			if (tag.startsWith("/")) {
+				if (buffer.isEmpty()) return false;
+				else {
+					if (!tag.substring(1).equals(buffer.pop())) return false;
+				}
+			} else {
+				buffer.push(tag);
+			}
+			j = html.indexOf('<', k + 1);
+		}
+		return buffer.isEmpty();
+	}
+
 	public static void main(String[] args) {
 		String[] words = { "adrianus", "ramunitz", "dexiciul" };
 		reverse(words);
@@ -60,5 +80,10 @@ public class StackUtils {
 		System.out.println(isMatched("({[])}"));
 		System.out.println(isMatched(" ("));
 		System.out.println(isMatched(" )"));
+
+		System.out.println(isHTMLValid("<html><head></head><body></body></html>"));
+		System.out.println(isHTMLValid("<html><head></head><body></html>"));
+		System.out.println(isHTMLValid("<html><head><body></head></body></html>"));
+		System.out.println(isHTMLValid("<html><head></head></body></html>"));
 	}
 }
