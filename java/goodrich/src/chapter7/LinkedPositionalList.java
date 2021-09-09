c4a6d32bddb88482c1911913b39555e2b4eb42d2
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-09 13:54:19 +0300

    implementation for a double linked positional list and the positional list and position interfaces

---
+++ java/goodrich/src/chapter7/LinkedPositionalList.java
@@ -0,0 +1,149 @@
+package chapter7;
+
+public class LinkedPositionalList<E> implements PositionalList<E> {
+	private static class Node<E> implements Position<E> {
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+
+		public Node(E e, Node<E> p, Node<E> n) {
+			element = e;
+			prev = p;
+			next = n;
+		}
+		
+		@Override
+		public E getElement() throws IllegalStateException {
+			if (next == null) throw new IllegalStateException("Position no longer valid.");
+			return element;
+		}
+		
+		public Node<E> getPrev() {
+			return prev;
+		}
+		
+		public Node<E> getNext() {
+			return next;
+		}
+		
+		public void setElement(E e) {
+			element = e;
+		}
+
+		public void setPrev(Node<E> p) {
+			prev = p;
+		}		
+		
+		public void setNext(Node<E> n) {
+			next = n;
+		}
+	}
+	
+	private Node<E> header;
+	private Node<E> tailer;
+	private int size = 0;
+	
+	public LinkedPositionalList() {
+		header = new Node<E>(null, null, null);
+		tailer = new Node<E>(null, header, null);
+		header.setNext(tailer);
+	}
+	
+	private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+		if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid position");
+		Node<E> node = (Node<E>)p;
+		if (node.getNext() == null)
+			throw new IllegalArgumentException("Position is no longer in the list");
+		return node;
+	}
+	
+	private Position<E> position(Node<E> node) {
+		if (node == header || node == tailer) return null;
+		return node;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public Position<E> first() {
+		return position(header.getNext());
+	}
+
+	@Override
+	public Position<E> last() {
+		return position(tailer.getPrev());
+	}
+
+	@Override
+	public Position<E> before(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return position(node.getPrev());
+	}
+
+	@Override
+	public Position<E> after(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return position(node.getNext());
+	}
+	
+	private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
+		Node<E> newest = new Node<E>(e, pred, succ);
+		pred.setNext(newest);
+		succ.setPrev(newest);
+		size++;
+		return newest;
+	}
+
+	@Override
+	public Position<E> addFirst(E e) {
+		return addBetween(e, header, header.getNext());
+	}
+
+	@Override
+	public Position<E> addLast(E e) {
+		return addBetween(e, tailer.getPrev(), tailer);
+	}
+
+	@Override
+	public Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return addBetween(e, node.getPrev(), node);
+	}
+
+	@Override
+	public Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return addBetween(e, node, node.getNext());
+	}
+
+	@Override
+	public E set(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		E answer = node.getElement();
+		node.setElement(e);
+		return answer;
+	}
+
+	@Override
+	public E remove(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		Node<E> predecessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predecessor.setNext(successor);
+		successor.setPrev(predecessor);
+		size--;
+		E answer = node.getElement();
+		node.setElement(null);
+		node.setNext(null);
+		node.setPrev(null);
+		return answer;
+	}
+}
---
+++ java/goodrich/src/chapter7/LinkedPositionalListTest.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+public class LinkedPositionalListTest {
+
+	public static void main(String[] args) {
+		LinkedPositionalList<String> names = new LinkedPositionalList<String>();
+		names.addFirst("bebe dex");
+		names.addBefore(names.first(), "ramunic");
+		Position<String> cursor = names.first();
+		while(cursor != null) {
+			System.out.println(cursor.getElement());
+			cursor = names.after(cursor);
+		}
+	}
+}
