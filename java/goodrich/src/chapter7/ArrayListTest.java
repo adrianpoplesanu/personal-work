eff0b5ed719f699f62a51b98969b91dbbb114f49
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-13 14:14:28 +0300

    tests for iterators

--- java/goodrich/src/chapter7/ArrayListTest.java
+++ java/goodrich/src/chapter7/ArrayListTest.java
@@ -1,26 +1,42 @@
 package chapter7;
 
+import java.util.Iterator;
+
 public class ArrayListTest {
     public static void main(String[] args) {
-    	ArrayList<String> list = new ArrayList<String>();
-    	list.add(0, "buna");
-    	list.add(1, "dimineata");
-    	list.add(2, "bebe");
-    	list.add(3, "dex");
+    	ArrayList<String> words = new ArrayList<String>();
+    	words.add(0, "buna");
+    	words.add(1, "dimineata");
+    	words.add(2, "bebe");
+    	words.add(3, "dex");
     	
     	/*list.add(0, "buna");
     	list.add(0, "dimineata");
     	list.add(0, "bebe");
     	list.add(0, "dex");*/
     	
-    	for (int i = 0; i < list.getSize(); i++) {
-    		System.out.println(list.get(i));
+    	for (int i = 0; i < words.getSize(); i++) {
+    		System.out.println(words.get(i));
     	}
     	
-    	list.remove(2);
+    	words.remove(2);
     	
-    	for (int i = 0; i < list.getSize(); i++) {
-    		System.out.println(list.get(i));
+    	for (int i = 0; i < words.getSize(); i++) {
+    		System.out.println(words.get(i));
+    	}
+    	
+    	System.out.println("=== Iterator test ============ ");
+    	
+    	Iterator<String> iter = words.iterator();
+    	while(iter.hasNext()) {
+    		String word = iter.next();
+    		System.out.println(word);
+    	}
+    	
+    	System.out.println("=== Iterator test #2 ========= ");
+    	
+    	for (String word : words) {
+    		System.out.println(word);
     	}
     }
 }
