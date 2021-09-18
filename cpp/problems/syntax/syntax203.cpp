673cc07d34b95db4428eefba819caeea36e95ac5
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-18 08:21:22 +0300

    parsing a dynamic allocated matrix

--- cpp/problems/syntax/syntax203.cpp
+++ cpp/problems/syntax/syntax203.cpp
@@ -8,6 +8,13 @@ void print(int *a, int n, int m) {
     }
 }
 
+void print2(int *a, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) std::cout << a[i * m + j] << " ";
+        std::cout << "\n";
+    }
+}
+
 int main(int argc, char *argv[]) {
     int n = 2, m = 5;
     int a[n][m];
@@ -15,7 +22,7 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < m; j++) a[i][j] = i * m + j;
     }
 
-    print(*a, n, m);
+    print2(*a, n, m);
 
     return 0;
 }
