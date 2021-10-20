4ee639302650d7d39f494577ff9d483342b8d847
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-10-20 15:52:31 +0300

    placeholder for DoublePositionalLinkedlList

---
+++ java/goodrich/src/adrian2/DoublePositionalLinkedlList.java
@@ -0,0 +1,85 @@
+package adrian2;
+
+import java.util.Iterator;
+
+public class DoublePositionalLinkedlList<E> implements PositionalList<E>, Iterable<E> {
+
+	@Override
+	public E first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addFirst(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLast(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(int i, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E set(int i, E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addAfter(Position<E> p, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addBefore(Position<E> p, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
