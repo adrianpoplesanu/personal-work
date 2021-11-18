b9d5da54b4c237f54552876513a160901dec3bbb
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-11-19 01:44:55 +0200

    fixed heap[poz / 2] to heap[(poz - 1) / 2] and added all the commands including print

--- cpp/problems/sda/heap1/heap.cpp
+++ cpp/problems/sda/heap1/heap.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int min() {
+int min(int heap[100], int size) {
+    if (size > 0) {
+        return heap[size - 1];
+    }
     return 0;
 }
 
@@ -11,16 +14,23 @@ int removeMin(int heap[100], int &size) {
 void insert(int heap[100], int &size, int e) {
     heap[size++] = e;
     int poz = size - 1;
-    while(poz >= 0) {
-        if (heap[poz / 2] > heap[poz]) {
+    while(poz > 0) {
+        if (heap[(poz - 1) / 2] > heap[poz]) {
             int aux = heap[poz];
-            heap[poz] = heap[poz / 2];
-            heap[poz / 2] = aux;
-            poz = poz / 2;
+            heap[poz] = heap[(poz - 1) / 2];
+            heap[(poz - 1) / 2] = aux;
+            poz = (poz - 1) / 2;
         } else {
             break;
         }
     }    
+}
+
+void print(int heap[100], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << heap[i] << " ";
+    }
+    std::cout << "\n";
 }
 
 int main(int argc, char *argv[]) {
@@ -35,7 +45,13 @@ int main(int argc, char *argv[]) {
             std::cin >> var;
             insert(heap, size, var);
         } else if (command == "print") {
-        
+            print(heap, size);
+        } else if (command == "min") {
+            std::cout << min(heap, size);
+        } else if (command == "remove") {
+
+        } else if (command == "exit") {
+            break;
         }
     }
     return 0;
