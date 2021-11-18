9de3ee9fc81cdf13e5e98283228895abde5dafa7
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-11-18 12:01:07 +0200

    Deque and CircularQueue interfaces

---
+++ java/goodrich/src/chapter6/CircularQueue.java
@@ -0,0 +1,5 @@
+package chapter6;
+
+public interface CircularQueue<E> extends Queue<E> {
+    void rotate();
+}
---
+++ java/goodrich/src/chapter6/Josephus.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class Josephus {
+
+	public static void main(String[] args) {
+		// TODO implement circular linked list queue, and then the logic for Josephus
+	}
+}
