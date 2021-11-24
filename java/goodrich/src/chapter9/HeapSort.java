5028da5c4c2d8f75bfc905514edd1df93c0e23d9
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-11-24 12:17:59 +0200

    pqSort using a heap

--- java/goodrich/src/chapter9/HeapSort.java
+++ java/goodrich/src/chapter9/HeapSort.java
@@ -1,9 +1,11 @@
 package chapter9;
 
+import chapter7.LinkedPositionalList;
+import chapter7.Position;
 import chapter7.PositionalList;
 
 public class HeapSort {
-    public static <E> void pqSoer(PositionalList<E> S, PriorityQueue<E, ?> P) {
+    public static <E> void pqSort(PositionalList<E> S, PriorityQueue<E, ?> P) {
         int n = S.size();
         for (int j = 0; j < n; j++) {
             E element = S.remove(S.first());
@@ -16,6 +18,20 @@ public class HeapSort {
     }
 
     public static void main(String[] args) {
-
+        PositionalList<Integer> numbers = new LinkedPositionalList<>();
+        numbers.addLast(4);
+        numbers.addLast(6);
+        numbers.addLast(9);
+        numbers.addLast(2);
+        numbers.addLast(3);
+        numbers.addLast(5);
+        numbers.addLast(1);
+        numbers.addLast(8);
+        numbers.addLast(7);
+        PriorityQueue<Integer, Integer> pq = new HeapPriorityQueue<>();
+        pqSort(numbers, pq);
+        for (Position<Integer> e : numbers.positions()) {
+            System.out.print(e.getElement() + " ");
+        }
     }
 }
