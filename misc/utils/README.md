4f403569ad3ef8b898241f712b2c253c68a107d9
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-12-05 22:13:06 +0200

    various helpful commands for investigating sites

---
+++ misc/utils/README.md
@@ -0,0 +1,8 @@
+Usage:
+
+$ . bash64.sh text.txt
+
+# get adrianus.ro, -k means ignore certificate, -vvv is super verbose
+$ . get-url
+
+$ whois adrianus.ro
---
+++ misc/utils/get-curl.sh
@@ -0,0 +1,1 @@
+curl -vvv -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36" -k "adrianus.ro"
