4f61ca38b2ab14a15c1fcea1d4524b65226f4bae
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-12-07 21:53:18 +0200

    rendered html from controller string - the only way this worked for me, PlainView doesn't do that for me

--- java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Controllers/Status/StatusController.java
+++ java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Controllers/Status/StatusController.java
@@ -2,8 +2,12 @@ package com.adrianpoplesanu.AnifScheduler.Controllers.Status;
 
 import com.adrianpoplesanu.AnifScheduler.Models.Status.StatusModel;
 import com.adrianpoplesanu.AnifScheduler.Views.PlainView;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class StatusController {
@@ -22,8 +26,8 @@ public class StatusController {
         return new StatusModel(200,"Everything looks fine!");
     }
 
-    @GetMapping("/test-view")
-    PlainView test_view() {
-        return new PlainView();
+    @GetMapping("/test-view-old")
+    ModelAndView test_view(Model model, HttpServletRequest request) {
+        return new ModelAndView(new PlainView());
     }
 }
--- java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Views/PlainView.java
+++ java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Views/PlainView.java
@@ -2,7 +2,6 @@ package com.adrianpoplesanu.AnifScheduler.Views;
 
 import org.springframework.web.servlet.view.AbstractView;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
@@ -11,7 +10,10 @@ import java.util.Map;
 public class PlainView extends AbstractView {
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ServletOutputStream out = response.getOutputStream();
-        out.println("<html><body><h1>Buna dimineata!</h1></body></html>");
+        PrintWriter out = response.getWriter();
+        //out.println("<!doctype html>");
+        //out.println("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head><body><h1>Buna dimineata!</h1></body></html>");
+        out.print("<html>Buna dimineata!</html>");
+        out.flush();
     }
 }
