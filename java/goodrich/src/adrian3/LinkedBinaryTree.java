f86646442de550d809c1411261e9119f67819bbd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-12-07 15:55:36 +0200

    LinkedBinaryTree needs to implement root() method from Tree inherited through AbstractTree

--- java/goodrich/src/adrian3/LinkedBinaryTree.java
+++ java/goodrich/src/adrian3/LinkedBinaryTree.java
@@ -4,6 +4,11 @@ import java.util.Iterator;
 
 public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
     @Override
+    public Position<E> root() {
+        return null;
+    }
+
+    @Override
     public Iterator<E> iterator() {
         return null;
     }
