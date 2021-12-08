3ceb7c39ff375ed82b33e9d89c5b6ee64f1a340c
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2021-12-08 17:41:55 +0200

    ANIF demo using springboot, mysql and cassandra, both running from within docker containers

---
+++ java/demo-anif2/DemoAnif2/src/main/java/com/adrianus/DemoAnif2/DemoAnif2Application.java
@@ -0,0 +1,13 @@
+package com.adrianus.DemoAnif2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAnif2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAnif2Application.class, args);
+	}
+
+}
---
+++ java/demo-anif2/DemoAnif2/src/test/java/com/adrianus/DemoAnif2/DemoAnif2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianus.DemoAnif2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAnif2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
