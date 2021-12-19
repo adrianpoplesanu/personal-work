0b8ff3bef44c9e23272ec2bf94c96b95c414679a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-12-20 01:29:12 +0200

    a simple reminder app

---
+++ java/Reminders/Reminders/src/main/java/com/adrianpoplesanu/Reminders/RemindersApplication.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.Reminders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindersApplication.class, args);
+	}
+
+}
