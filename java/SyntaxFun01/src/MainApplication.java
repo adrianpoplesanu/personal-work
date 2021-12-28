7dea727fdab40e19dbd4a1f80d4ed308a9dc757d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-12-28 16:16:39 +0200

    predicate functional interface fun

--- java/SyntaxFun01/src/MainApplication.java
+++ java/SyntaxFun01/src/MainApplication.java
@@ -1,9 +1,14 @@
 import com.adrianpoplesanu.function01.FunctionPointerTest;
+import com.adrianpoplesanu.function01.PredicatePointerTest;
 
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("running all syntax fun snippets...");
         FunctionPointerTest functionPointerTest = new FunctionPointerTest();
         functionPointerTest.testFunctionMethod();
+
+        PredicatePointerTest predicatePointerTest = new PredicatePointerTest();
+        predicatePointerTest.testPredicatePointer();
+        predicatePointerTest.testPredicatePointer2();
     }
 }
---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/function01/PredicatePointerTest.java
@@ -0,0 +1,27 @@
+package com.adrianpoplesanu.function01;
+
+import java.util.function.Predicate;
+
+public class PredicatePointerTest {
+    Predicate<String> callback;
+
+    private boolean filterName(String name) {
+        return name.contains("adrianus");
+    }
+
+    private boolean filterPet(String name) {
+        return name.contains("bedush");
+    }
+
+    public void testPredicatePointer() {
+        callback = this::filterName;
+        System.out.println(callback.test("adrianus e gibutza"));
+    }
+
+    public void testPredicatePointer2() {
+        callback = this::filterName;
+        Predicate<String> callback2 = this::filterPet;
+        System.out.println(callback.and(callback2).test("adrianus per scorillor"));
+        System.out.println(callback.and(callback2).test("adrianus si bedush"));
+    }
+}
