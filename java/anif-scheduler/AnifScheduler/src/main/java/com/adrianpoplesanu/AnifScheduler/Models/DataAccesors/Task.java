5f77d54e74aca1a17e393f016e8212b3e2fc5698
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-01-01 15:57:04 +0200

    added User and Task Entities and thymleaf pom dependency

---
+++ java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Models/DataAccesors/Task.java
@@ -0,0 +1,34 @@
+package com.adrianpoplesanu.AnifScheduler.Models.DataAccesors;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table
+public class Task {
+    @Column
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private int userid;
+
+    @Column
+    private String description;
+
+    @Column
+    private String status;
+
+    @Column
+    private String seap_status;
+
+    @Column(name = "createddate", insertable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "modifieddate", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedDate;
+}
---
+++ java/anif-scheduler/AnifScheduler/src/main/java/com/adrianpoplesanu/AnifScheduler/Models/DataAccesors/User.java
@@ -0,0 +1,35 @@
+package com.adrianpoplesanu.AnifScheduler.Models.DataAccesors;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Column
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String firstname;
+
+    @Column
+    private String lastname;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column(name = "createddate", insertable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Column(name = "modifieddate", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedDate;
+
+}
