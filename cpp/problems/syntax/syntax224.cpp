408a94d9f767b731cecb4994f134ec20f2ea357a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-01-14 23:57:54 +0200

    config parsing fun syntax

--- cpp/problems/syntax/syntax224.cpp
+++ cpp/problems/syntax/syntax224.cpp
@@ -5,11 +5,17 @@
 int main(int argc, char *argv[]) {
     std::string config = "key1:value1,key2:value2";
     std::map<std::string, std::string> configMap;
-    std::stringstream stream(config);
-    while(stream.good()) {
+    std::stringstream config_stream(config);
+    while(config_stream.good()) {
         std::string substr;
-        getline(stream, substr, ',');
-        std::cout << substr << "\n";
+        getline(config_stream, substr, ',');
+        //std::cout << substr << "\n";
+        std::stringstream entry_stream(substr);
+        while (entry_stream.good()) {
+            std::string substr2;
+            getline(entry_stream, substr2, ':');
+            std::cout << substr2 << "\n";
+        }
     }
     return 0;
 }
