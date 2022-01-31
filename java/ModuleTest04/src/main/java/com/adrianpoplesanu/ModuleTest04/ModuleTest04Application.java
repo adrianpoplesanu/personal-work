25255cdf8408e227c3d764df4fd8a58e004010db
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-02-01 00:42:55 +0200

    simple value retrieval from application.properties

---
+++ java/ModuleTest04/src/main/java/com/adrianpoplesanu/ModuleTest04/ModuleTest04Application.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.ModuleTest04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTest04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleTest04Application.class, args);
+	}
+
+}
