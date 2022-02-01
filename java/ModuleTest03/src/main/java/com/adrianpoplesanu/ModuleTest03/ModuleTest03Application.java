40081dbfa8d69a32be26ab008e9a10d137aa436a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-02-02 01:42:07 +0200

    module test #3

---
+++ java/ModuleTest03/src/main/java/com/adrianpoplesanu/ModuleTest03/ModuleTest03Application.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.ModuleTest03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTest03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleTest03Application.class, args);
+	}
+
+}
---
+++ java/ModuleTest03/src/test/java/com/adrianpoplesanu/ModuleTest03/ModuleTest03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.ModuleTest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleTest03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
