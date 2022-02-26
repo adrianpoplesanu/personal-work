12a226c050081a7abc821ccc7b2096a89c7a52c4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-02-26 22:22:12 +0200

    online reservations #2

---
+++ java/spring_boot/OnlineReservations2/src/test/java/com/adrianpoplesanu/OnlineReservations2/OnlineReservations2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.OnlineReservations2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineReservations2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
