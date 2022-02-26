2525c668a4fffb59c852e2c6be00710202cf5847
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-02-26 22:20:59 +0200

    algo execution

---
+++ java/spring_boot/AlgoExecution/src/test/java/com/adrian/poplesanu/AlgoExecution/AlgoExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.poplesanu.AlgoExecution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
