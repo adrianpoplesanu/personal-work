34cdb1cd273b825bf311ac53a58e8719fc879f26
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-03-05 05:07:23 +0200

    stream parsing fun

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/parsers/ParserChecker.java
@@ -0,0 +1,42 @@
+package com.adrianpoplesanu.parsers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ParserChecker {
+    public static void test() {
+        /*ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);*/
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        //numbers.stream().forEach(e -> { if(e % 2 == 0) result.add(e);});
+        numbers.stream().filter(e -> e % 2 == 0).forEach(e -> result.add(e));
+
+        System.out.println(result);
+    }
+
+    public static List<Integer> test2(List<Integer> numbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (numbers.stream().filter(e -> e == 6).findAny().isPresent()) return result;
+        numbers.forEach(e -> result.add(e));
+        return result;
+    }
+
+    public static void test3() {
+        List<Integer> numbers = Arrays.asList(1, 2, 6, 7);
+        System.out.println(test2(numbers));
+        numbers = Arrays.asList(1, 3, 5, 7);
+        System.out.println(test2(numbers));
+    }
+
+    public static void main(String[] args) {
+        test3();
+    }
+}
