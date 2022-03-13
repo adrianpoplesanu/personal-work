ff95871f8c05d11e809df17d6479abd47324559e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-03-13 05:25:41 +0200

    simple stack machine logic

--- java/StackMachine/src/StackMachine.java
+++ java/StackMachine/src/StackMachine.java
@@ -1,5 +1,16 @@
+import virtualmachine.VirtualMachine;
+
 public class StackMachine {
     public static void main(String[] args) {
-        //...
+        String[] commands = {
+                "PUSH", "3",
+                "PUSH", "4",
+                "ADD",
+                "PUSH", "5",
+                "MINUS"
+        };
+
+        VirtualMachine vm = new VirtualMachine(commands);
+        vm.execute();
     }
 }
--- java/StackMachine/src/virtualmachine/VirtualMachine.java
+++ java/StackMachine/src/virtualmachine/VirtualMachine.java
@@ -4,45 +4,72 @@ import java.util.List;
 import java.util.Stack;
 
 public class VirtualMachine {
+    private String[] commands;
     private Stack<Integer> stack;
+    private int programCounter;
+    private int stackPointer;
 
     public VirtualMachine() {
         stack = new Stack<Integer>();
+        programCounter = 0;
+        stackPointer = 0;
     }
 
     public VirtualMachine(String[] commands) {
         this();
-        execute(commands);
+        this.commands = commands;
     }
 
     public VirtualMachine(List<String> commands) {
         this();
-        execute(commands);
+        this.commands = (String[]) commands.toArray();
     }
 
-    private void execute(String[] commands) {
-        int i = 0;
-        while(i < commands.length) {
-
+    public void execute() {
+        programCounter = 0;
+        stackPointer = 0;
+        while(programCounter < commands.length) {
+            switch(commands[programCounter]) {
+                case "PUSH":
+                    push(commands[programCounter + 1]);
+                    break;
+                case "ADD":
+                    add();
+                    break;
+                case "MINUS":
+                    minus();
+                    break;
+            }
+            programCounter++;
         }
+        printStacktop();
     }
 
-    private void execute(List<String> commands) {
-        int i = 0;
-        while(i < commands.size()) {
-            
-        }
-    }
-
-    private void push() {
-        // ...
+    private void push(String number) {
+        stack.push(Integer.valueOf(number));
+        programCounter++;
+        stackPointer++;
     }
 
     private void add() {
-        // ...
+        int right = stack.pop();
+        stackPointer--;
+        int left = stack.pop();
+        stackPointer--;
+        stack.push(right + left);
+        stackPointer++;
     }
 
     private void minus() {
-        // ...
+        int right = stack.pop();
+        stackPointer--;
+        int left = stack.pop();
+        stackPointer--;
+        stack.push(left - right);
+        stackPointer++;
+    }
+
+    private void printStacktop() {
+        System.out.println("Stacktop: " + stack.peek() + " StackPointer: " + stackPointer);
     }
 }
