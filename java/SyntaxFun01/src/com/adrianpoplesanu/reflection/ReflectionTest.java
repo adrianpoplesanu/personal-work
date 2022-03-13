fa4811ea0c9a6877898b32d9b13b38156f2f01ca
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-03-13 11:04:00 +0200

    stub for some reflection work

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/reflection/ReflectionTest.java
@@ -0,0 +1,11 @@
+package com.adrianpoplesanu.reflection;
+
+public class ReflectionTest {
+    public void test() {
+        //...
+    }
+
+    public static void main(String[] args) {
+        System.out.println("buna dimineata...");
+    }
+}
