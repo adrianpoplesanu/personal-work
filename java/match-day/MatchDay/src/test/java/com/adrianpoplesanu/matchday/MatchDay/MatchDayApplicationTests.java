a4bc682020c4374ce4394ab359d6e95ad3bdf940
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-03-25 10:45:46 +0200

    initial match day commit

---
+++ java/match-day/MatchDay/src/test/java/com/adrianpoplesanu/matchday/MatchDay/MatchDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.matchday.MatchDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
