0baf65a99013decaea7a0e5285a0fedda37b272f
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2022-03-25 10:54:43 +0200

    super simple, super fast back algo

--- cpp/problems/miscellaneous/back01.cpp
+++ cpp/problems/miscellaneous/back01.cpp
@@ -1,5 +1,30 @@
+// permutations
 #include <iostream>
 
+bool check(int i, int a[]) {
+    for (int j = 0; j < i; j++) if (a[j] == a[i]) return false;
+    return true;
+}
+
+void back(int i, int n, int a[]) {
+    if (i == n) {
+        for (int j = 0; j < n; j++) std::cout << a[j] << " ";
+        std::cout << "\n";
+    } else {
+        a[i] = 1;
+        while(a[i] <= n) {
+            if (check(i, a)) {
+                back(i + 1, n, a);
+            }
+            a[i]++;
+        }
+        a[i] = 0;
+    }
+}
+
 int main(int argc, char *argv[]) {
+    int n = 3;
+    int a[n];
+    back(0, n, a);
     return 0;
 }
