6e039a1eeed4d95243f3179af037337ed5c31b9e
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2022-04-02 02:39:25 +0300

    added dependencies for parallel execution

--- kotelin/parallel01/build.gradle.kts
+++ kotelin/parallel01/build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.1")
 }
 
 tasks.test {
