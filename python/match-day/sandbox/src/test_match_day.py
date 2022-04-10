44c3f67f1302e47b856436155d14a7c1402ef35b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-10 18:51:51 +0300

    match day test script

---
+++ python/match-day/sandbox/src/test_match_day.py
@@ -0,0 +1,12 @@
+#import requests
+#import json
+
+print('testing match-day connection...')
+
+response = requests.get(url='https://api.football-data.org/v2/competitions', headers={"X-Auth-Token": "f5c1f45ccb174b739541ad43203c7183"})
+
+#response = requests.get(url='https://api.football-data.org/v2/competitions/EC/matches', headers={"X-Auth-Token": "f5c1f45ccb174b739541ad43203c7183"})
+
+data = json.loads(response.text)
+
+print(data)
