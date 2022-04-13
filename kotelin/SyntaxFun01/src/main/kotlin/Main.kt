750a2f709eb6bba5b98f7f211e1b2a2506389857
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-14 00:47:11 +0300

    ?: operator and String? syntax fun

--- kotelin/SyntaxFun01/src/main/kotlin/Main.kt
+++ kotelin/SyntaxFun01/src/main/kotlin/Main.kt
@@ -3,10 +3,10 @@ import elvis.Elvis
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    //println("Program arguments: ${args.joinToString()}")
+    println("Program arguments: ${args.joinToString()}")
 
     val elvis = Elvis();
     elvis.test()
+    elvis.test2()
+    elvis.test3(null)
 }
--- kotelin/SyntaxFun01/src/main/kotlin/elvis/Elvis.kt
+++ kotelin/SyntaxFun01/src/main/kotlin/elvis/Elvis.kt
@@ -2,7 +2,16 @@ package elvis
 
 class Elvis {
     fun test() {
-        val a : String = "neata";
-        println(a);
+        val a : String = "neata"
+        println(a)
+    }
+
+    fun test2() {
+        val a = null
+        println(a ?: "aaa")
+    }
+
+    fun test3(a : String?) {
+        println(a)
     }
 }
