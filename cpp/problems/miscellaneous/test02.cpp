5ef3dcb07854b8040c1d75f66e0ab203e320eeea
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-18 18:45:36 +0300

    proper tasks.json and lauch.json for having a console asociated with the project

---
+++ cpp/problems/miscellaneous/test02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int a = 5;
+    a += 6;
+    std::cout << a << "\n";
+    return 0;
+}
---
+++ cpp/problems/miscellaneous/test03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int a;
+    a = 5;
+    std::cin >> a;
+    a++;
+    std::cout << a << "\n";
+    return 0;
+}
