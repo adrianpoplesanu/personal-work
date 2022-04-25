cecb4bd391a12c061e2ea323ddc60a5e6cbe15fd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-26 00:54:04 +0300

    bitwise fun

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/bitwise/Bitwise.java
@@ -0,0 +1,11 @@
+package com.adrianpoplesanu.bitwise;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a << 1);
+
+        int b = 16;
+        System.out.println(b >> 1);
+    }
+}
