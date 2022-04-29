15333c42dbaca6998eec6c5a4d994c06050864e9
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-30 00:32:47 +0300

    big number load and print functionality

--- java/BigNumbers2/src/big_number/BigNumber.java
+++ java/BigNumbers2/src/big_number/BigNumber.java
@@ -4,21 +4,33 @@ public class BigNumber {
     private int[] digits = new int[100];
     private int[] floatDigits = new int[100];
     private static final int NUMBER_BASE = 10000;
+    private int size;
 
     public void load(String source) {
         int len = source.length();
         int start = len - 4, end = len;
+        int i = 0;
         while (end > 0) {
+            if (start < 0) start = 0;
             String digit = source.substring(start, end);
-            System.out.println(digit);
+            digits[i] = Integer.valueOf(digit);
             start -= 4;
             end -= 4;
+            i++;
+        }
+        size = i;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println("digit" + i + ": " + digits[i]);
         }
     }
 
     public static void main(String[] args) {
         System.out.println("testing BigNumber object implementation, delete when done...");
         BigNumber bigNumber = new BigNumber();
-        bigNumber.load("111122223333");
+        bigNumber.load("11122223333");
+        bigNumber.print();
     }
 }
