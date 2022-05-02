0d5009741a2e2ae863c4975733d789d790d7535b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-05-02 11:00:05 +0300

    webflux reactive webclient

---
+++ java/WebClient1/src/main/java/WebClientApplication.java
@@ -0,0 +1,99 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import model.ApiResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class WebClientApplication {
+
+    private static void test() {
+        final int size = 16 * 1024 * 1024;
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+        // https://stackoverflow.com/questions/59735951/databufferlimitexception-exceeded-limit-on-max-bytes-to-buffer-webflux-error
+
+        WebClient webClient2 = WebClient.builder()
+                .exchangeStrategies(strategies)
+                .baseUrl("https://api.football-data.org")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Auth-Token", "f5c1f45ccb174b739541ad43203c7183")
+                .build();
+
+        JsonNode jsonNode = webClient2.get()
+                .uri("/v2/competitions/PL/matches")
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .block();
+
+        System.out.println(jsonNode);
+    }
+
+    private static void test2() {
+        final int size = 16 * 1024 * 1024;
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+        // https://stackoverflow.com/questions/59735951/databufferlimitexception-exceeded-limit-on-max-bytes-to-buffer-webflux-error
+
+        WebClient webClient2 = WebClient.builder()
+                .exchangeStrategies(strategies)
+                .baseUrl("https://api.football-data.org")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Auth-Token", "f5c1f45ccb174b739541ad43203c7183")
+                .build();
+
+        Mono<String> monoResult = webClient2.get()
+                .uri("/v2/competitions/PL/matches")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        String text = monoResult.block();
+
+        System.out.println(text);
+    }
+
+    private static void test3() {
+        final int size = 16 * 1024 * 1024;
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+        // https://stackoverflow.com/questions/59735951/databufferlimitexception-exceeded-limit-on-max-bytes-to-buffer-webflux-error
+
+        WebClient webClient2 = WebClient.builder()
+                .exchangeStrategies(strategies)
+                .baseUrl("https://api.football-data.org")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Auth-Token", "f5c1f45ccb174b739541ad43203c7183")
+                .build();
+
+        Mono<ApiResponse> monoResult = webClient2.get()
+                .uri("/v2/competitions/PL/matches")
+                .retrieve()
+                .bodyToMono(ApiResponse.class);
+
+        ApiResponse response = monoResult.block();
+
+        System.out.println(response);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("running WebClientApplication");
+
+        test3();
+        // https://www.baeldung.com/spring-5-webclient
+        // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-webflux
+
+        /*WebClient client = WebClient.create("https://api.football-data.org");
+        // /v2/competitions/PL/matches
+
+        Mono<Response> responseMono = client.get()
+                .uri("/v2/competitions/{id}/matches", "PL")
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        responseMono.subscribe(response -> System.out.println(response));*/
+    }
+}
---
+++ java/WebClient1/src/main/java/model/ApiResponse.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ApiResponse {
+    private Match[] matches;
+
+    public Match[] getMatches() {
+        return matches;
+    }
+
+    public void setMatches(Match[] matches) {
+        this.matches = matches;
+    }
+}
