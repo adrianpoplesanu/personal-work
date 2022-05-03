60f41de650e44128e8ca74bfca013eea7962d0ee
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-05-03 21:54:53 +0300

    a simpler dirtier way of doing get calls

--- java/WebClient1/src/main/java/WebClientApplication.java
+++ java/WebClient1/src/main/java/WebClientApplication.java
@@ -6,7 +6,34 @@ import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WebClientApplication {
+
+    private static void test0() throws IOException {
+        // https://chillyfacts.com/java-send-http-getpost-request-and-read-json-response/
+        String url = "https://api.football-data.org/v2/competitions/PL/matches";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("X-Auth-Token", "f5c1f45ccb174b739541ad43203c7183");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println(response.toString());
+    }
 
     private static void test() {
         final int size = 16 * 1024 * 1024;
@@ -79,7 +106,7 @@ public class WebClientApplication {
         System.out.println(response);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("running WebClientApplication");
 
         test3();
