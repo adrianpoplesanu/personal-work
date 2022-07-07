21717a0e7dc8b77126772958e4d57b7b12b5a6c3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-07-08 01:43:49 +0300

    more precedences

--- java/VmImplementation2/src/parser/PrecedenceType.java
+++ java/VmImplementation2/src/parser/PrecedenceType.java
@@ -3,7 +3,12 @@ package parser;
 public enum PrecedenceType {
     LOWEST,
     ASSIGN,
+    EQUALS,
+    CONDITIONAL,
+    LESSGREATER,
     SUM,
     MULTIPLY,
-    PREFIX
+    PREFIX,
+    CALL,
+    INDEX
 }
