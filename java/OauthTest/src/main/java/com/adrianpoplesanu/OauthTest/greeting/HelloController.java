67aadbe86380f6ba427c2d81956ab7d99692a040
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-07-18 04:02:04 +0300

    printing jwt token used to login + pom updates for needed libraries

--- java/OauthTest/src/main/java/com/adrianpoplesanu/OauthTest/greeting/HelloController.java
+++ java/OauthTest/src/main/java/com/adrianpoplesanu/OauthTest/greeting/HelloController.java
@@ -3,11 +3,17 @@ package com.adrianpoplesanu.OauthTest.greeting;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -18,7 +24,14 @@ import static org.springframework.http.HttpStatus.OK;
 public class HelloController {
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseStatus(OK)
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello World");
+    public ResponseEntity<String> sayHello(Principal principal) {
+        //final OAuth2AuthenticationDetails details =
+        //        (OAuth2AuthenticationDetails) auth.getDetails();
+
+        //token
+        //String accessToken = details.getTokenValue();
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
+
+        return ResponseEntity.ok("Bearer: " + token.getToken().getTokenValue());
     }
 }
