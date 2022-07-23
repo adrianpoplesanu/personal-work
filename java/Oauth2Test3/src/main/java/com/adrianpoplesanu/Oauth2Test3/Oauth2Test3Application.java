82d4fb5d29ef88eb9254a2fda12bb246d9b5fcd7
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2022-07-23 18:02:23 +0300

    yet another oauth2 test project

---
+++ java/Oauth2Test3/src/main/java/com/adrianpoplesanu/Oauth2Test3/Oauth2Test3Application.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.Oauth2Test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Test3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Test3Application.class, args);
+	}
+
+}
---
+++ java/Oauth2Test3/src/test/java/com/adrianpoplesanu/Oauth2Test3/Oauth2Test3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianpoplesanu.Oauth2Test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2Test3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
