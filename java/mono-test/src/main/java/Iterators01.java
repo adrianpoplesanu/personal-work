a3ecc9b1b0fb671d97f623d897865981d26fa347
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-09-10 13:53:19 +0300

    more iterator tests

--- java/mono-test/src/main/java/Iterators01.java
+++ java/mono-test/src/main/java/Iterators01.java
@@ -47,6 +47,16 @@ public class Iterators01 {
         System.out.println(info);
     }
 
+    private static void testImbricatedFluxCartesianProduct2() {
+        Flux<String> namesFlux = Flux.just("adish", "bedush");
+        Flux<String> addressesFlux = Flux.just("Bdul Buc Noi", "Str Anton Bacal", "Oltenitei");
+
+        Flux<String> infoFlux = addressesFlux.flatMap(address -> namesFlux.map(x -> x + ": " + address));
+
+        List<String> info = infoFlux.collectList().block();
+        System.out.println(info);
+    }
+
     private static void testThen() {
         Mono<String> nameMono = Mono.just("adish");
         List<String> addresses = Arrays.asList("Bdul Buc Noi", "Str Anton Bacal", "Oltenitei");
@@ -69,12 +79,33 @@ public class Iterators01 {
         monoResult.block();
     }
 
+    private static void testFluxMap() {
+        Flux<String> addresses = Flux.just("Bdul Buc Noi", "Str Anton Bacal", "Oltenitei");
+        Flux<String> upperNameAddresses = addresses.map(x -> {
+            System.out.println("here");
+            return x.toUpperCase();
+        });
+        System.out.println(upperNameAddresses.collectList().block());
+    }
+
+    private static void testFluxFlatMap() {
+        Flux<String> addresses = Flux.just("Bdul Buc Noi", "Str Anton Bacal", "Oltenitei");
+        Flux<String> upperNameAddresses = addresses.flatMap(address -> {
+            System.out.println("here");
+            return Flux.just(address.toUpperCase());
+        });
+        System.out.println(upperNameAddresses.collectList().block());
+    }
+
     public static void main(String[] args) {
         testImbricatedIterators();
         testImbricatedCartesianProduct();
         testImbricatedFlux();
         testImbricatedFluxCartesianProduct();
+        testImbricatedFluxCartesianProduct2();
         testThen();
         testThenWithImbricatedFlux();
+        testFluxMap();
+        testFluxFlatMap();
     }
 }
