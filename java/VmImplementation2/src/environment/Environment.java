85b5d08c8e4914a990751b927e63d9e1564875fe
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-09-17 16:05:05 +0300

    minor evaluator and environment changes

--- java/VmImplementation2/src/environment/Environment.java
+++ java/VmImplementation2/src/environment/Environment.java
@@ -28,7 +28,39 @@ public class Environment {
 
     public AdObject get(String ident) {
         if (store.containsKey(ident)) return store.get(ident);
-        if (outer != null && outer.store.containsKey(ident)) return outer.store.get(ident);
+        //if (outer != null && outer.store.containsKey(ident)) return outer.store.get(ident);
+        if (outer != null) return outer.get(ident);
         return null;
+    }
+
+    public boolean containsInStore(String ident) {
+        return store.containsKey(ident);
+    }
+
+    public boolean containsInOuter(String ident) {
+        return outer.store.containsKey(ident);
+    }
+
+    public boolean containsInUpstream(String ident) {
+        if (store.containsKey(ident)) {
+            return true;
+        } else if (outer != null) {
+            return outer.containsInUpstream(ident);
+        }
+        return false;
+    }
+
+    public void addValueToStore(String ident, AdObject obj) {
+        // declaring a variable explicit in scope triggers this, just line params passed through to a function
+        store.put(ident, obj);
+    }
+
+    public void updateValueInUpstream(String ident, AdObject obj) {
+        // this helps to re-assign-ing global or at least two level up variables
+        if (store.containsKey(ident)) {
+            store.put(ident, obj);
+        } else {
+            outer.updateValueInUpstream(ident, obj);
+        }
     }
 }
--- java/VmImplementation2/src/evaluator/Evaluator.java
+++ java/VmImplementation2/src/evaluator/Evaluator.java
@@ -6,6 +6,20 @@ import objects.AdObject;
 
 public class Evaluator {
     public AdObject eval(AstNode node, Environment environment) {
+        switch(node.getType()) {
+            case PROGRAM:
+                return evalProgram(node, environment);
+            case INTEGER:
+                return evalInteger(node, environment);
+        }
+        return null;
+    }
+
+    private AdObject evalProgram(AstNode node, Environment environment) {
+        return null;
+    }
+
+    private AdObject evalInteger(AstNode node, Environment environment) {
         return null;
     }
 }
