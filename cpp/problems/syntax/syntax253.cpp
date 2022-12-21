45ee1f6c2d33456af50e34a655de0968341eae9a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-12-21 19:20:06 +0200

    error when forward declaration of class is used as parent class

--- cpp/problems/syntax/syntax253.cpp
+++ cpp/problems/syntax/syntax253.cpp
@@ -1,4 +1,15 @@
+// forward declaration does not work for inheriting or any operation that needs contextual information
+// forward declaration just tells the compiler that a class with that name __will__ exist
 #include <iostream>
+
+class B;
+class A : B {
+
+};
+
+class B {
+
+};
 
 int main(int argc, char* argv[]) {
     std::cout << "checking class ordering & inheritancce operator\n";
