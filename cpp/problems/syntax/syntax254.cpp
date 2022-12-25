3aa5812e7ff7e03f790d51c9e20eb93d133f6417
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-12-25 21:31:21 +0200

    more env reference testing with inner env*

--- cpp/problems/syntax/syntax254.cpp
+++ cpp/problems/syntax/syntax254.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <map>
+
+class Environment {
+public:
+    std::map<std::string, std::string> store;
+    std::map<std::string, Environment*> siblings;
+
+    Environment() {
+        store.insert(std::pair<std::string, std::string>("token0", "bebe"));
+        store.insert(std::pair<std::string, std::string>("token1", "dex"));
+        store.insert(std::pair<std::string, std::string>("token2", "e"));
+        store.insert(std::pair<std::string, std::string>("token3", "drabutz"));
+    }
+};
 
 int main(int argc, char* argv[]) {
-    std::cout << "bebe dex e drabutz\n";
+    std::cout << "testing map with Environment*\n";
+    Environment *env = new Environment();
     return 0;
 }
