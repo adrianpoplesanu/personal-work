909a3ec509170c6a5030a048f38c0afa4739b1ed
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-01-08 00:26:24 +0200

    a simple adrianus purpose app, the jar is running on adiranus but traffic is not redirected

---
+++ java/AdrianusTesting/src/main/java/ro/adrianus/AdrianusTesting/AdrianusTestingApplication.java
@@ -0,0 +1,13 @@
+package ro.adrianus.AdrianusTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrianusTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrianusTestingApplication.class, args);
+	}
+
+}
---
+++ java/AdrianusTesting/src/test/java/ro/adrianus/AdrianusTesting/AdrianusTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.adrianus.AdrianusTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrianusTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
