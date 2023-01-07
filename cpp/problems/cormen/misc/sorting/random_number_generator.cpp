5db3a386325910ed70f7c8c20b2e89991c3ec8d1
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-01-07 13:11:48 +0200

    sorting algorithms for testing execution time

---
+++ cpp/problems/cormen/misc/sorting/random_number_generator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int random(int min, int max) {
+   static bool first = true;
+   if (first) {
+      srand( time(NULL) );
+      first = false;
+   }
+   return min + rand() % (( max + 1 ) - min);
+}
+
+int main(int argc, char* argv[]) {
+    int n = 1000;
+    for (int i = 0; i < n; i++) {
+        std::cout << random(0, 9);
+        if (i != n - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "\n";
+}
