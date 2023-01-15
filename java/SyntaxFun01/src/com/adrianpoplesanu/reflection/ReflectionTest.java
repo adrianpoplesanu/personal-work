439caff3159adc6b91f0c08f450f63fb0113efc4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-01-15 02:22:58 +0200

    a simple reflexion script

--- java/SyntaxFun01/src/com/adrianpoplesanu/reflection/ReflectionTest.java
+++ java/SyntaxFun01/src/com/adrianpoplesanu/reflection/ReflectionTest.java
@@ -1,11 +1,24 @@
 package com.adrianpoplesanu.reflection;
 
+import java.lang.reflect.Method;
+import java.util.Stack;
+
 public class ReflectionTest {
-    public void test() {
-        //...
+    public static void test() {
+        try {
+            Class c = Class.forName("java.util.Stack");
+            Method m[] = c.getDeclaredMethods();
+            for (int i = 0; i < m.length; i++)
+                System.out.println(m[i].toString());
+        }
+        catch (Throwable e) {
+            System.err.println(e);
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("buna dimineata...");
+        test();
+        Stack<String> a;
     }
 }
