b0b538e748037cb221c52da43f31c8a974a30e72
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-02-02 23:51:08 +0200

    application for testing the proxy in adrianus

--- java/AdrianusTesting/src/main/java/ro/adrianus/AdrianusTesting/controller/HomeController.java
+++ java/AdrianusTesting/src/main/java/ro/adrianus/AdrianusTesting/controller/HomeController.java
@@ -1,16 +1,29 @@
 package ro.adrianus.AdrianusTesting.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
-    public String index() {
-        return "Adrianus running with Springboot!";
+    public String index(@RequestHeader Map<String, String> headers, @RequestParam(required=false) Map<String,String> qparams) {
+        String result = "<div>Adrianus running with Springboot!</div>";
+        result += "<div>--headers--</div>";
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            result += "<div>" + header.getKey() + ": " + header.getValue() + "</div>";
+        }
+        result += "<div>-/headers/-</div>";
+
+        result += "<div>--qparams--</div>";
+        for (Map.Entry<String, String> qparam : qparams.entrySet()) {
+            result += "<div>" + qparam.getKey() + ": " + qparam.getValue() + "</div>";
+        }
+        result += "<div>-/qparams--</div>";
+        return result;
     }
 }
