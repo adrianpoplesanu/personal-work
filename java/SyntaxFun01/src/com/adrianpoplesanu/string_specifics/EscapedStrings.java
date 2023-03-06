21406718afbc6573999da4dbf8047356fdf68df4
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-03-06 22:03:09 +0200

    simple escaping sequences

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/string_specifics/EscapedStrings.java
@@ -0,0 +1,9 @@
+package com.adrianpoplesanu.string_specifics;
+
+public class EscapedStrings {
+    public static void main(String[] args) {
+        System.out.println("\"");
+        System.out.println("\'");
+        System.out.println("\\");
+    }
+}
