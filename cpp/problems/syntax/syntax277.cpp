b44120d161ea2478b2851241a4e12795a27072bf
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-04-18 00:36:58 +0300

    ad structure for threads for testing seg faults

--- cpp/problems/syntax/syntax277.cpp
+++ cpp/problems/syntax/syntax277.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <map>
 
 enum Ad_Type {
     INTEGER
@@ -9,6 +10,8 @@ class Ad_Int {
 public:
     int value;
     Ad_Type type;
+    Ad_Int *next;
+    Ad_Int *prev;
 
     Ad_Int(int v) {
         type = INTEGER;
@@ -18,31 +21,47 @@ public:
 
 class Environment {
 public:
-    Ad_Int *contor;
+    std::map<std::string, Ad_Int*> store;
 
     Environment() {
-        contor = new Ad_Int(55);
+        //...
     }
 };
 
-void worker(Environment *env, int delay) {
+class GarbageCollector {
+public:
+    Ad_Int *head, *tail;
+    void addObject() {
+
+    }
+    void markObjects() {
+
+    }
+    void sweepObject() {
+
+    }
+};
+
+void worker(Environment *env, GarbageCollector *gc, int delay) {
     std::cout << "running a thread\n";
     std::cout << "sleeping for " << delay << "miliseconds\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-    std::cout << "type: " << env->contor->type;
-    std::cout << "contor: " << env->contor->value + 4;
-    delete env->contor;
-    std::cout << "finishing a thread" << "\n";
+    //Ad_Type type = env->store["aaa"]->type; // linia asta ridica genul de eroare pe care il gasesc si in ad
+    gc->markObjects();
+    gc->sweepObject();
+    //delete env->contor;
+    std::cout << "finishing a thread\n";
 }
 
 int main(int argc, char *argv[]) {
 
     Environment *env = new Environment();
+    GarbageCollector *gc = new GarbageCollector();
 
-    std::thread th1(worker, env, 10);
-    std::thread th2(worker, env, 20);
-    std::thread th3(worker, env, 30);
-    std::thread th4(worker, env, 40);
+    std::thread th1(worker, env, gc, 10);
+    std::thread th2(worker, env, gc, 20);
+    std::thread th3(worker, env, gc, 30);
+    std::thread th4(worker, env, gc, 40);
 
 
     th1.join();
