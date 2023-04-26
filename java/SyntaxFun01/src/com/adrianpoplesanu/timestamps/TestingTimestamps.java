ce712e4a9a9f54ca769b219f4da65b27c1994b06
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-04-26 17:54:08 +0300

    testing timestamps #1

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/timestamps/TestingTimestamps.java
@@ -0,0 +1,8 @@
+package com.adrianpoplesanu.timestamps;
+
+public class TestingTimestamps {
+    public static void main(String[] args) {
+        // This method returns the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC(coordinated universal time).
+        System.out.println(System.currentTimeMillis());
+    }
+}
