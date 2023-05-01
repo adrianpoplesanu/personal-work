8e080f24e45f909307119f2a3823bea0aa99f5e1
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-05-01 16:31:15 +0300

    code cleanup

--- java/SyntaxFun01/src/com/adrianpoplesanu/encoding/TestEncoding1.java
+++ java/SyntaxFun01/src/com/adrianpoplesanu/encoding/TestEncoding1.java
@@ -7,7 +7,6 @@ public class TestEncoding1 {
         byte[] decoded = Base64.getUrlDecoder().decode("LIPwImNG5vovjmwDOIZsHWxSIYRDOFaQbEGfRFM9RgA=");
         String result = "[";
         for (int i = 0; i < decoded.length; i++) {
-            //System.out.println(decoded[i]);
             if (i == decoded.length - 1) {
                 result += decoded[i];
             } else {
