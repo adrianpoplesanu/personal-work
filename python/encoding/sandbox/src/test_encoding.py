f0d1b6bcd3dc3821a0f3c1aafb90c6483b7294ae
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-05-01 16:36:34 +0300

    base64 decode, store & format byte array

--- python/encoding/sandbox/src/test_encoding.py
+++ python/encoding/sandbox/src/test_encoding.py
@@ -1,1 +1,11 @@
 import base64
+
+text = "LIPwImNG5vovjmwDOIZsHWxSIYRDOFaQbEGfRFM9RgA="
+b64_decoded = base64.b64decode(text)
+byte_arr_unformatted = bytearray(b64_decoded)
+byte_arr_formatted = [x for x in byte_arr_unformatted]
+
+print(text)
+print(b64_decoded)
+print(byte_arr_unformatted)
+print(byte_arr_formatted)
