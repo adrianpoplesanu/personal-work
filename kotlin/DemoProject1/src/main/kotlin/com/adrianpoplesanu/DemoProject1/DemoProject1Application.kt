c0e71f698637cd2b41c295a66e508490ec21a440
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-05-30 13:14:30 +0300

    kotlin spring boot 2.7.12 project template

---
+++ kotlin/DemoProject1/src/main/kotlin/com/adrianpoplesanu/DemoProject1/DemoProject1Application.kt
@@ -0,0 +1,11 @@
+package com.adrianpoplesanu.DemoProject1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoProject1Application
+
+fun main(args: Array<String>) {
+	runApplication<DemoProject1Application>(*args)
+}
