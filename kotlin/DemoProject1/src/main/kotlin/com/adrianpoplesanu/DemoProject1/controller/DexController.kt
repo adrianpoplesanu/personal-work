c8965b4986c77f52c3ccfd84309648ebe975aa90
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-05-31 18:16:28 +0300

    added post for more proxy tests

--- kotlin/DemoProject1/src/main/kotlin/com/adrianpoplesanu/DemoProject1/controller/DexController.kt
+++ kotlin/DemoProject1/src/main/kotlin/com/adrianpoplesanu/DemoProject1/controller/DexController.kt
@@ -2,13 +2,20 @@ package com.adrianpoplesanu.DemoProject1.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller()
 class DexController {
     @GetMapping("/dex")
     @ResponseBody
-    fun dexIndex() : String {
+    fun getDex() : String {
         return "bebe dex"
+    }
+
+    @PostMapping("/dex")
+    @ResponseBody
+    fun createDex() : String {
+        return "bebe dex post"
     }
 }
