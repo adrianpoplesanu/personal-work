a127eaadf5a89a5ae94cab64198ed8043f94b380
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-06-28 21:36:47 +0300

    a raw test for @Ignore and @Test

---
+++ java/TestingDependencies/src/test/java/TestNGRaw.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+@Ignore
+public class TestNGRaw {
+    @Test
+    public void testSomething123() {
+        System.out.println("testSomething123()");
+    }
+}
