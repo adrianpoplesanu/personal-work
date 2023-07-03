817c4be8009db0a098e71330f2cdbfcf06f9ee82
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-07-03 23:38:42 +0300

    added more logic for proxy testing

--- kotlin/DemoProject1/src/main/kotlin/com/adrianpoplesanu/DemoProject1/controller/DexController.kt
+++ kotlin/DemoProject1/src/main/kotlin/com/adrianpoplesanu/DemoProject1/controller/DexController.kt
@@ -3,6 +3,7 @@ package com.adrianpoplesanu.DemoProject1.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller()
@@ -10,12 +11,12 @@ class DexController {
     @GetMapping("/dex")
     @ResponseBody
     fun getDex() : String {
-        return "bebe dex"
+        return "bebe dex <form method=\"POST\"><input type=\"hidden\" id=\"fname\" name=\"fname\" value=\"some text here\"></input><button type=\"submit\">Click here for POST</button></form>"
     }
 
     @PostMapping("/dex")
     @ResponseBody
-    fun createDex() : String {
-        return "bebe dex post"
+    fun createDex(@RequestBody fname: String) : String {
+        return "bebe dex post: $fname"
     }
 }
