951703cb3f3aaf8fb9de998629eb7ae7f409c7af
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-07-12 19:44:50 +0300

    concurency in kotlin book

---
+++ kotlin/BookConcurencyInKotlin01/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "BookConcurencyInKotlin01"
+
---
+++ kotlin/BookConcurencyInKotlin01/src/main/kotlin/MainApplication.kt
@@ -0,0 +1,9 @@
+/*class MainApplication {
+    fun main(args : Array<String>) {
+        println("buna dimineata")
+    }
+}*/
+
+fun main(args : Array<String>) {
+    println("buna dimineata")
+}
