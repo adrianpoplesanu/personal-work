f4b230a049eea9cf7f6c460ef9a95574a35dd26d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-07-15 17:44:34 +0300

    learning concurency in kotlin with maven

---
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/HelloConcurencyWorld.kt
@@ -0,0 +1,22 @@
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+suspend fun createCoroutines(amount : Int) {
+    val jobs = ArrayList<Job>()
+    for (i in 1..amount) {
+        jobs += GlobalScope.launch {
+            delay(1000)
+        }
+    }
+    jobs.forEach {
+        it.join()
+    }
+}
+
+fun main(args : Array<String>) = runBlocking {
+    val time = measureTimeMillis {
+        createCoroutines(10_000)
+    }
+
+    println("Took $time ms")
+}
