b6698a656b3431813b83b0b95cb800f460f704bc
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-07-19 19:20:41 +0300

    moved thing around

--- kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/HelloConcurencyWorld.kt
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/HelloConcurencyWorld.kt
@@ -4,6 +4,7 @@ import kotlin.system.measureTimeMillis
 suspend fun createCoroutines(amount : Int) {
     val jobs = ArrayList<Job>()
     for (i in 1..amount) {
+        // in the book launch is used here, I had to use GlobalScope
         jobs += GlobalScope.launch {
             delay(1000)
         }
