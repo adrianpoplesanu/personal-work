31847a3c3e4593e7741409c682062b0ccb4590f6
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-08-13 09:14:08 +0300

    print map keys and values

--- cpp/problems/syntax/syntax282.cpp
+++ cpp/problems/syntax/syntax282.cpp
@@ -1,7 +1,19 @@
 // sorting a map by its string keys
 
 #include <iostream>
+#include <map>
 
 int main(int argc, char *argv[]) {
+    std::map<std::string, std::string> store;
+
+    store["d"] = "bedus";
+    store["b"] = "e";
+    store["c"] = "catelus";
+    store["a"] = "dragutz";
+
+    for(std::map<std::string, std::string>::const_iterator it = store.begin(); it != store.end(); ++it) {
+        std::cout << it->first << ": " << it->second << "\n";
+    }
+
     return 0;
 }
