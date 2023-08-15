97ab5f7b9203af281f0ed968ab1b374a874bba4e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-08-15 09:00:20 +0300

    testing List.of

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/list1/ListTesting.java
@@ -0,0 +1,11 @@
+package com.adrianpoplesanu.list1;
+
+import org.testng.collections.Lists;
+
+import java.util.List;
+
+public class ListTesting {
+    public static void main(String[] args) {
+        //List<String> names = List.of("bebe", "dex"); // List.of nu exista
+    }
+}
