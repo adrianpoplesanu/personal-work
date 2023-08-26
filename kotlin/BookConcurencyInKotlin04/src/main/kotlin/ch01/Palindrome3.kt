4efd97e933a43874f1dcd1074e0819e7e07fef9a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-08-26 15:16:04 +0300

    loading words.json as resources

--- kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/Palindrome3.kt
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/Palindrome3.kt
@@ -15,12 +15,20 @@ class Palindrome3 {
         //return this::class.java.getResource(fileName)?.readText(Charsets.UTF_8) ?: "not found"
         return File(fileName).readText(Charsets.UTF_8)
     }
+
+    fun readWordsFromJson2(fileName : String) : String {
+        return this::class.java.getResource(fileName)?.readText(Charsets.UTF_8) ?: "not found"
+    }
 }
 
 fun main(args : Array<String>) {
     var palindrome3 = Palindrome3()
     val words = palindrome3.readWordsFromJson("/Users/adrianpoplesanu/Documents/git-projects/personal-work/kotlin/BookConcurencyInKotlin04/src/main/resources/words.json")
     println(words)
+
+    val words2 = palindrome3.readWordsFromJson2("/words.json")
+    println(words2)
+
     //val palindromes = palindrome3.filterPalindromes(words)
     //println(palindromes)
 }
