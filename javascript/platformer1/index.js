dc635b6fb84b4f69f60f416246187321a8404466
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-09-11 22:34:31 +0300

    a simple platformer

---
+++ javascript/platformer1/index.js
@@ -0,0 +1,14 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext('2d');
+var width = canvas.width;
+var height = canvas.height;
+
+function clear() {
+    context.beginPath();
+    //context.fillStyle = "black"; //"#556655";
+    context.fillStyle = "#556655";
+    context.rect(0, 0, width, height);
+    context.fill();
+}
+
+clear(); 
