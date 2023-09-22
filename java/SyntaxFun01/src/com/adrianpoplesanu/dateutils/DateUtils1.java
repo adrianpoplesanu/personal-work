f97404204b21f4e9f0a901de41228044d2eed9f5
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-09-22 15:42:03 +0300

    TimeUnit.DAYS.toSeconds test

---
+++ java/SyntaxFun01/src/com/adrianpoplesanu/dateutils/DateUtils1.java
@@ -0,0 +1,9 @@
+package com.adrianpoplesanu.dateutils;
+
+import java.util.concurrent.TimeUnit;
+
+public class DateUtils1 {
+    public static void main(String[] args) {
+        System.out.println(TimeUnit.DAYS.toSeconds(90)); // 7776000 secunde in 90 de zile
+    }
+}
