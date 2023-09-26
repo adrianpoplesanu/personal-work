7d1a333e3642e8612e11559a6ece51e8a9fc3a05
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-09-26 13:19:57 +0300

    seconds in a year value

--- java/SyntaxFun01/src/com/adrianpoplesanu/dateutils/DateUtils1.java
+++ java/SyntaxFun01/src/com/adrianpoplesanu/dateutils/DateUtils1.java
@@ -5,5 +5,6 @@ import java.util.concurrent.TimeUnit;
 public class DateUtils1 {
     public static void main(String[] args) {
         System.out.println(TimeUnit.DAYS.toSeconds(90)); // 7776000 secunde in 90 de zile
+        System.out.println(TimeUnit.DAYS.toSeconds(365)); // 31536000 secunde in 365 de zile
     }
 }
