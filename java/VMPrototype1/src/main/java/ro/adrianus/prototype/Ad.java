757ba6c847cd21976f8e1909617f108fa28b4a37
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-09-29 21:51:01 +0300

    vm prototype for Ad lang

---
+++ java/VMPrototype1/src/main/java/ro/adrianus/prototype/Ad.java
@@ -0,0 +1,11 @@
+package ro.adrianus.prototype;
+
+public class Ad {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+
+        } else {
+            System.out.println("Ad compiler prototype + VM [JAVA]... v0.1");
+        }
+    }
+}
