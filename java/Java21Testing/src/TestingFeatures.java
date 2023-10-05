787fd74b67a78541400cea145b39c7004bf9a2f2
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-10-05 15:39:33 +0300

    playing a bit with SequencedCollections

--- java/Java21Testing/src/TestingFeatures.java
+++ java/Java21Testing/src/TestingFeatures.java
@@ -1,7 +1,14 @@
+import java.util.List;
+
 public class TestingFeatures {
     public static void main(String[] args) {
         System.out.println("running java21...");
 
-        //List names = new L
+        List names = List.of("aaa", "bbb", "ccc", "ddd", "eee", "fff");
+        List rNames = names.reversed();
+        System.out.println(names);
+        System.out.println(rNames);
+
+        System.out.println(names == rNames.reversed());
     }
 }
