59b5acaf1a72c8e409589e5704b8c832de3fb8fa
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-10-29 22:45:11 +0200

    game loop work

--- javascript/game-server/game.js
+++ javascript/game-server/game.js
@@ -1,9 +1,45 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+var height = canvas.height;
+var width = canvas.width;
+
+var framesPerSecond = 30;
+
 function clearScreen() {
     context.fillStyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, width, height);
 }
 
-clearScreen();
+function GameLoop() {
+
+}
+
+GameLoop.prototype.clear = function () {
+    clearScreen();
+}
+
+GameLoop.prototype.update = function () {
+    this.clear();
+}
+
+var gameLoop = new GameLoop();
+
+function Game() {
+    this.gameObjects = [];
+}
+
+Game.prototype.start = function () {
+    console.log("starting game...");
+    setInterval(this.update, 1000/framesPerSecond);
+}
+
+Game.prototype.update = function () {
+    gameLoop.update();
+    for (gameObject in this.gameObjects) {
+        gameObject.update();
+    }
+}
+
+var game = new Game();
+game.start();
