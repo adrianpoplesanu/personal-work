e0438f51e4baa9e0360071c5e4f8757fd6b847bb
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-11-12 09:47:00 +0200

    comparing floats in cpp

---
+++ cpp/problems/syntax/syntax288.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    float a = 0.1;
+    float b = 0.1;
+    std::cout << (a != b) << "\n";
+    std::cout << (a == b) << "\n";
+    return 0;
+}
