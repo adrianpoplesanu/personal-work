8895e406f53f51071139e1fe8ac63fd04a216466
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-11-21 21:55:50 +0200

    defaulting an enum valueOf to a default value

---
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/syntax_fun/EnumTesting.kt
@@ -0,0 +1,27 @@
+package syntax_fun
+
+class EnumTesting {
+    enum class AnimalType {
+        DOG,
+        CAT,
+        HORSE,
+        UNKNOWN
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args : Array<String>) {
+            println("running enum testing...")
+            var animalType : AnimalType = AnimalType.valueOf("DOG")
+            println(animalType)
+            animalType = AnimalType.valueOf("CAT")
+            println(animalType)
+            try {
+                animalType = AnimalType.valueOf("CACA_MACA")
+            } catch (e: java.lang.Exception) {
+                animalType = AnimalType.UNKNOWN
+            }
+            println(animalType)
+        }
+    }
+}
