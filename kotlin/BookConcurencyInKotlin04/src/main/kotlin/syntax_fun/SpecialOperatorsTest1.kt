d2d2225391c09b916d53cbff53b64409f70d9660
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-11-22 17:45:09 +0200

    operator ?. usage

---
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/syntax_fun/SpecialOperatorsTest1.kt
@@ -0,0 +1,26 @@
+package syntax_fun
+
+class SpecialOperatorsTest1 {
+    fun generate(op: Int): String? {
+        if (op > 5) {
+            return "aaa"
+        }
+        return null
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("running SpecialOperatorsTest1...")
+            val specialOperatorsTest1 = SpecialOperatorsTest1()
+            var result = specialOperatorsTest1.generate(7)
+            result?.let {
+                println(it)
+            }
+            result = specialOperatorsTest1.generate(4)
+            result?.let {
+                println(it)
+            }
+        }
+    }
+}
