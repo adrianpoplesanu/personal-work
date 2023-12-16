99e165353a54290d2dd810e0cc5861947f1dbcd7
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-12-16 10:56:10 +0200

    added parsing functions

--- python/VMPrototype1/parser.py
+++ python/VMPrototype1/parser.py
@@ -6,15 +6,32 @@ class Parser:
     def __init__(self):
         self.source = None
         self.lexer = Lexer()
+        self.current_token = None
+        self.peek_token = None
         self.prefix_parse_fns = {}
         self.infix_parse_fns = {}
         self.prefix_parse_fns[TokenType.IDENT] = self.parse_identifier
 
     def load(self, source):
         self.source = source
+        self.lexer.load(source)
+        self.next_token()
+        self.next_token()
 
     def build_program_statements(self, program):
-        pass
+        while self.current_token.token_type != TokenType.EOF:
+            self.next_token()
+            pass
+
+    def next_token(self):
+        self.current_token = self.peek_token
+        self.peek_token = self.lexer.next_token()
+
+    def current_token_id(self, token_type : TokenType):
+        return self.current_token.token_type == token_type
+
+    def peek_token_is(self, token_type: TokenType):
+        return self.peek_token.token_type == token_type
 
     def parse_statement(self):
         pass
