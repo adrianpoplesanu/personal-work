87394156c8c4f57922c60a3f19a5bb278c762cc3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-12-30 02:04:06 +0200

    minor refactoring

--- javascript/gl-shader3/index.js
+++ javascript/gl-shader3/index.js
@@ -1,50 +1,50 @@
 console.log("bd engine running...");
 
 var canvas = document.querySelector("#canvas");
-var gl = canvas.getContext("webgl");
+var webgl = canvas.getContext("webgl");
 var program;
 
-function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+function createShader(webgl, type, source) {
+    var shader = webgl.createShader(type);
+    webgl.shaderSource(shader, source);
+    webgl.compileShader(shader);
+    var success = webgl.getShaderParameter(shader, webgl.COMPILE_STATUS);
     if (success) {
         return shader;
     }
 
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
+    console.log(webgl.getShaderInfoLog(shader));
+    webgl.deleteShader(shader);
 }
 
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+function createProgram(webgl, vertexShader, fragmentShader) {
+    var program = webgl.createProgram();
+    webgl.attachShader(program, vertexShader);
+    webgl.attachShader(program, fragmentShader);
+    webgl.linkProgram(program);
+    var success = webgl.getProgramParameter(program, webgl.LINK_STATUS);
     if (success) {
         return program;
     }
 
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
+  console.log(webgl.getProgramInfoLog(program));
+  webgl.deleteProgram(program);
 }
 
 function init() {
     var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
     var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    var vertexShader = createShader(webgl, webgl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(webgl, webgl.FRAGMENT_SHADER, fragmentShaderSource);
 
-    program = createProgram(gl, vertexShader, fragmentShader);
+    program = createProgram(webgl, vertexShader, fragmentShader);
 
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionAttributeLocation = webgl.getAttribLocation(program, "a_position");
 
-    var positionBuffer = gl.createBuffer();
+    var positionBuffer = webgl.createBuffer();
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer);
 
     // END init
 
@@ -54,33 +54,33 @@ function init() {
         0.7, 0,
     ];
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(positions), webgl.STATIC_DRAW);
 
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    webgl.viewport(0, 0, webgl.canvas.width, webgl.canvas.height);
 
     // clear the canvas
-    gl.clearColor(0.1, 0.1, 0.1, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    webgl.clearColor(0.1, 0.1, 0.1, 1.0);
+    webgl.clear(webgl.COLOR_BUFFER_BIT);
 
-    gl.useProgram(program);
+    webgl.useProgram(program);
 
-    gl.enableVertexAttribArray(positionAttributeLocation);
+    webgl.enableVertexAttribArray(positionAttributeLocation);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer);
 
     var size = 2;          // 2 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floats
+    var type = webgl.FLOAT;   // the data is 32bit floats
     var normalize = false; // don't normalize the data
     var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
     var offset = 0;        // start at the beginning of the buffer
-    gl.vertexAttribPointer(
+    webgl.vertexAttribPointer(
         positionAttributeLocation, size, type, normalize, stride, offset);
 
     // draw
-    var primitiveType = gl.TRIANGLES;
+    var primitiveType = webgl.TRIANGLES;
     var offset = 0;
     var count = 3;
-    gl.drawArrays(primitiveType, offset, count);
+    webgl.drawArrays(primitiveType, offset, count);
 }
 
 init();
