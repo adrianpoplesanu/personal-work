de82afb845849a03fcd9f960dbdaf5f69c51a276
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-01-03 15:13:03 +0200

    added object type

--- python/VMPrototype1/objects.py
+++ python/VMPrototype1/objects.py
@@ -1,7 +1,12 @@
+class AdObjectType:
+    INT = 0
+
+
 class AdObject:
     pass
 
 
 class AdObjectInteger(AdObject):
     def __init__(self, value: int):
+        self._type = AdObjectType.INT
         self.value = value
