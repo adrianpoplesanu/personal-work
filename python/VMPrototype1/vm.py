4ed9d83b8b4b0236a751035363136d81871bec72
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-01-06 00:18:42 +0200

    vm load push and pop implementation

--- python/VMPrototype1/vm.py
+++ python/VMPrototype1/vm.py
@@ -1,13 +1,29 @@
-class VM:
-    def __init__(self):
-        pass
+from bytecode import Bytecode
+from objects import AdObject
 
-    def load(self, bytecode):
-        pass
+
+class VM:
+    def __init__(self, instructions=None, constants=None):
+        self.instructions = instructions
+        self.constants = constants
+        self.stack = []
+        self.sp = 0
+
+    def load(self, bytecode: Bytecode):
+        self.instructions = bytecode.instructions
+        self.constants = self.constants
 
     def run(self):
         pass
 
     def last_popped_stack_element(self):
         pass
-    
+
+    def push(self, obj: AdObject):
+        self.sp += 1
+        self.stack.append(obj)
+
+    def pop(self) -> AdObject:
+        result = self.stack[self.sp - 1]
+        self.sp -= 1
+        return result
