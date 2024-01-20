b192ed49f1d2e4a9cece2ea5ab0089f14e4f4bb0
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-01-20 12:05:30 +0200

    printing current thread name with dispatcher name

--- kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch02/DispatcherTesting1.kt
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch02/DispatcherTesting1.kt
@@ -1,10 +1,24 @@
 package ch02
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 class DispatcherTesting1 {
+    fun printCurrentThread() {
+        println("Running in thread [${Thread.currentThread().name}]")
+    }
+    suspend fun run() {
+        val task = GlobalScope.launch {
+            printCurrentThread()
+        }
+        task.join()
+    }
     companion object {
         @JvmStatic
-        fun main(args: Array<String>) {
-            println("running...")
+        fun main(args: Array<String>) = runBlocking {
+            val dispatcherTesting1 = DispatcherTesting1()
+            dispatcherTesting1.run()
         }
     }
 }
