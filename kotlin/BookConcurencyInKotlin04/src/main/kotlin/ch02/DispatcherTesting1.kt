aaea3bb05f27840abb7f51899524fdb07d92618c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-01-20 11:57:43 +0200

    placeholder for testing dispatcher

---
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch02/DispatcherTesting1.kt
@@ -0,0 +1,10 @@
+package ch02
+
+class DispatcherTesting1 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("running...")
+        }
+    }
+}
