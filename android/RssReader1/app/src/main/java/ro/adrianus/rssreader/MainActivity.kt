22a5631be8c4c59fb2d8db8e467de59954c563a5
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-01-27 01:41:51 +0200

    added document parsing logic for retrieving headlines

--- android/RssReader1/app/src/main/java/ro/adrianus/rssreader/MainActivity.kt
+++ android/RssReader1/app/src/main/java/ro/adrianus/rssreader/MainActivity.kt
@@ -13,9 +13,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
 import ro.adrianus.rssreader.ui.theme.RssReader1Theme
 import java.net.URL
-import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : ComponentActivity() {
@@ -32,15 +33,23 @@ class MainActivity : ComponentActivity() {
             }
         }
         GlobalScope.launch(dispatcher) {
-            // TODO call coroutine here
-            fetchAdrianusHomepage()
+            val headlines = fetchRssHeadlines()
+            println(headlines)
         }
     }
 
     private fun fetchRssHeadlines(): List<String> {
         val builder = factory.newDocumentBuilder()
         val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
-        return emptyList()
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length)
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map { it as Element }
+            .filter { "item" == it.tagName }
+            .map {
+                it.getElementsByTagName("title").item(0).textContent
+            }
     }
 
     private fun fetchAdrianusHomepage() {
