fc7a1bc3505c1c55e6b14844854385be7b3fc194
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-02-10 01:33:10 +0200

    practicing again with a 1k array size

---
+++ cpp/problems/syntax/syntax296.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+const int BASE = 10000;
+const int SIZE = 1000;
+
+void init(int a[SIZE]) {
+    for (int i = 0; i < SIZE; i++) a[i] = 0;
+}
+
+void copy(int a[SIZE], int b[SIZE]) {
+    for (int i = 0; i < SIZE; i++) b[i] = a[i];
+}
+
+void print_digit(int val) {
+    if (val < 10) {
+        std::cout << "000" << val;
+    } else if (val < 100) {
+        std::cout << "00" << val;
+    } else if (val < 1000) {
+        std::cout << "0" << val;
+    } else {
+	std::cout << val;
+    }
+}
+
+void print(int a[SIZE]) {
+    int found = false;
+    for (int i = SIZE - 1; i >= 0; i--) {
+        if (!found) {
+            if (a[i] != 0) {
+                found = true;
+                std::cout << a[i];
+            }
+        } else {
+            print_digit(a[i]);
+        }
+    }
+}
+
+void sum(int a[SIZE], int b[SIZE], int c[SIZE]) {
+    int remainder = 0;
+    for (int i = 0; i < SIZE; i++) {
+         c[i] = a[i] + b[i] + remainder;
+         remainder = 0;
+         while (c[i] >= BASE) {
+             c[i] -= BASE;
+             remainder++;
+         }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int a[SIZE], b[SIZE], c[SIZE];
+    init(a);
+    init(b);
+    init(c);
+
+    b[0] = 1;
+
+    int i = 0;
+    while (i < 15000) {
+        sum(a, b, c);
+        copy(b, a);
+        copy(c, b);
+        i++;
+    }
+
+    print(a);
+    std::cout << "\n";
+
+    return 0;
+}
