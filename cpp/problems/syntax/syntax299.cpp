ed765dd553191c4c78c75ad7b1107fda44147ba3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-02-18 00:50:11 +0200

    testing run time for allocating memory with new operator

---
+++ cpp/problems/syntax/syntax299.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+
+int main(int argc, char *argv[]) {
+    clock_t begin = clock();
+
+    int i = 0;
+    while(i < 100000) {
+        int *a = new int();
+        i++;
+        delete a;
+    }
+
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "ran for: " << elapsed_secs << "sec\n";
+    return 0;
+}
