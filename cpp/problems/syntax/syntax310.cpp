332120fac3550d8a0bed95b61bbc013d56fc394b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-03-28 00:04:34 +0200

    pointer variable asign with printing to console

---
+++ cpp/problems/syntax/syntax310.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+#include <memory>
+
+int main(int argc, char *argv[]) {
+
+    clock_t start = clock(); 
+
+    for (int i = 0; i < 1000000; i++) {
+        int *value = new int(5);
+        (*value)++;
+        std::cout << *value << "\n";
+        delete value;
+    }
+
+    clock_t end = clock();
+    std::cout << "ran for: " << double(end - start) / CLOCKS_PER_SEC << "\n";
+
+    return 0;
+}
+
+// ran for: 0.546828
