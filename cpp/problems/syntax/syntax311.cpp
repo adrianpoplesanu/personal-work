e52afa72c2f9cc29a2e25afe02e5ea26ac7cd69b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-03-29 00:21:45 +0200

    more polymorphism fun

--- cpp/problems/syntax/syntax311.cpp
+++ cpp/problems/syntax/syntax311.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <vector>
 
 class Base {
-protected:
-    int type = 77; // this is default
 public:
+    int type = 77; // this is default
     int age = 66;
+
+    void fromBase(Base b) {
+        type = b.type;
+        age = b.age;
+    }
+
+    Base toBase() {
+        Base b;
+        b.age = age;
+        b.type = type;
+        return b;
+    }
 };
 
 class Parent : public Base {
@@ -26,6 +38,24 @@ void change(Base base) {
     base.age = 88;
 }
 
+Base generate() {
+    Parent parent;
+    parent.age = 44;
+    parent.setType(3);
+    return parent;
+}
+
+std::vector<Base> generateElements() {
+    std::vector<Base> elements;
+    Parent p1;
+    p1.age = 22;
+    elements.push_back(p1);
+    Parent p2;
+    p2.type = 999;
+    elements.push_back(p2);
+    return elements;
+}
+
 int main(int argc, char *argv[]) {
     Parent parent;
     parent.print();
@@ -34,5 +64,17 @@ int main(int argc, char *argv[]) {
     change(parent);
     parent.print();
     parent.printAge();
+    Parent generated;
+    generated.fromBase(generate());
+    generated.print();
+    generated.printAge();
+
+    std::vector<Base> results = generateElements();
+    for (int i = 0; i < 2; i++) {
+        Parent p;
+        p.fromBase(results[i]);
+        p.print();
+        p.printAge();
+    }
     return 0;
 }
