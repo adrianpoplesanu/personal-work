6ba539cb8f03a77242feccaca24d45cb6b0063a0
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-04-11 00:33:41 +0300

    overriding delete operator for specific class

--- cpp/problems/syntax/syntax315.cpp
+++ cpp/problems/syntax/syntax315.cpp
@@ -6,10 +6,15 @@ void* operator new(size_t size) {
     return malloc(size);
 }
 
-void operator delete(void *memory) {
-    std::cout << "deallocatingi "<< sizeof(memory) << " bytes\n";
+void operator delete(void *memory) noexcept {
+    std::cout << "deallocating "<< sizeof(memory) << " bytes\n";
     free(memory);
 }
+
+//void operator delete(Test *aaa) {
+//    std::cout << "aaa\n";
+//    free(aaa);
+//}
 
 //void operator delete(void *memory, size_t size) {
 //    std::cout << "deallocating " << size << " bytes\n";
@@ -24,11 +29,21 @@ public:
         b = 66;
         c = 77;
     }
+    void operator delete(void* ptr) {
+        std::cout << "deallocating within class: " << sizeof(*((Test*)ptr)) << " bytes\n";
+        free(ptr);
+    }
 };
+
+//void operator delete(Test *aaa) {
+//    std::cout << "aaa\n";
+//    free(aaa);
+//}
 
 int main(int argc, char *argv[]) {
     int *a = new int(5);
     Test *test = new Test();
+    std::cout << "sizeof *test: " << sizeof(test) << " bytes\n";
     //std::unique_ptr<Test> obj = std::make_unique<Test>();
     delete a;
     delete test;
