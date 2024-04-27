d470e4f093ad6c2bb48f3682762082b5a25b9922
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-04-28 01:15:07 +0300

    added OP_POP op code byte

--- python/VMPrototype1/code_ad.py
+++ python/VMPrototype1/code_ad.py
@@ -46,6 +46,7 @@ class Code:
             OpCodeByte.OP_MINUS: Definition("OpMinus", 0, []),
             OpCodeByte.OP_MULTIPLY: Definition("OpMultiply", 0, []),
             OpCodeByte.OP_DIVIDE: Definition("OpDivide", 0, []),
+            OpCodeByte.OP_POP: Definition("OpPop", 0, []),
             OpCodeByte.OP_TRUE: Definition("OpTrue", 0, []),
             OpCodeByte.OP_FALSE: Definition("OpFalse", 0, []),
         }
