535f1a2433140ac0c25d7897e1e767a1a0c1286e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-04-27 07:56:18 +0300

    created definitions for true and false op codes

--- python/VMPrototype1/code_ad.py
+++ python/VMPrototype1/code_ad.py
@@ -46,6 +46,8 @@ class Code:
             OpCodeByte.OP_MINUS: Definition("OpMinus", 0, []),
             OpCodeByte.OP_MULTIPLY: Definition("OpMultiply", 0, []),
             OpCodeByte.OP_DIVIDE: Definition("OpDivide", 0, []),
+            OpCodeByte.OP_TRUE: Definition("OpTrue", 0, []),
+            OpCodeByte.OP_FALSE: Definition("OpFalse", 0, []),
         }
 
     def make(self, opcode: OpCode, n, args):
