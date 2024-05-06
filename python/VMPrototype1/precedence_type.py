2c05243285acd839c4185a0e79610d31986bced7
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-05-07 00:11:00 +0300

    added precedence types for equals, not equals and greater than

--- python/VMPrototype1/precedence_type.py
+++ python/VMPrototype1/precedence_type.py
@@ -3,6 +3,8 @@ from token_type import TokenType
 
 class PrecedenceType:
     LOWEST = 0
+    EQUALS = 3
+    LESSGREATER = 5
     SUM = 6
     PRODUCT = 7
     PREFIX = 9
@@ -12,5 +14,9 @@ precedences = {
     TokenType.PLUS: PrecedenceType.SUM,
     TokenType.MINUS: PrecedenceType.SUM,
     TokenType.SLASH: PrecedenceType.PRODUCT,
-    TokenType.ASTERISK: PrecedenceType.PRODUCT
+    TokenType.ASTERISK: PrecedenceType.PRODUCT,
+    TokenType.EQ: PrecedenceType.EQUALS,
+    TokenType.NOT_EQ: PrecedenceType.EQUALS,
+    TokenType.GT: PrecedenceType.LESSGREATER,
+    TokenType.LT: PrecedenceType.LESSGREATER
 }
