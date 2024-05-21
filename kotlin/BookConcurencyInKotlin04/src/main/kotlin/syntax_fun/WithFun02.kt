5cbf604fdfed542162a5d3118c64f9a5f3e87ee9
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-05-21 21:02:52 +0300

    simple decorators that don't work if return is added

---
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/syntax_fun/WithFun02.kt
@@ -0,0 +1,24 @@
+package syntax_fun
+
+class ExampleClass {
+    fun exampleMethod(): Int = withTimestamp {
+        // Do something here
+        val result = (1..1000).sum() // Example operation
+        result
+    }
+
+    fun anotherMethod(): String = withTimestamp {
+        // Another example operation
+        Thread.sleep(100) // Simulating some work
+        "Hello, World!"
+    }
+}
+
+fun main() {
+    val example = ExampleClass()
+    val result1 = example.exampleMethod()
+    println("Result of exampleMethod(): $result1")
+
+    val result2 = example.anotherMethod()
+    println("Result of anotherMethod(): $result2")
+}
