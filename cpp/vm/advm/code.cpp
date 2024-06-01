c1f8db14ed1bd308dc540957af196b8e78d9c600
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-06-02 00:27:35 +0300

    added definition for greather than

--- cpp/vm/advm/code.cpp
+++ cpp/vm/advm/code.cpp
@@ -13,6 +13,7 @@ Code::Code() {
     definitionsMap.insert(std::make_pair(OP_FALSE, new Definition("OpFalse", 0, new int)));
     definitionsMap.insert(std::make_pair(OP_GREATERTHAN, new Definition("OpGreaterThan", 0, new int)));
     definitionsMap.insert(std::make_pair(OP_EQUALS, new Definition("OpEquals", 0, new int)));
+    definitionsMap.insert(std::make_pair(OP_GREATERTHANEQUAL, new Definition("OpGreaterThanEqual", 0, new int)));
 }
 
 Code::~Code() {
