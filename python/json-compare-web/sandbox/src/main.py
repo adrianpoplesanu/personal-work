5454ff6f08416fc4a1475138c44ddfa33fe038fb
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-07-01 18:00:39 +0300

    response comparator initial commit

---
+++ python/json-compare-web/sandbox/src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+envs = ['env1', 'env2']
+hosts = {'env1': ['aaa.com', 'bbb.com'], 'env2': ['ccc.com', 'ddd.com']}
+
+@app.get("/compare")
+async def compare():
+    return {"message": "Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={'envs': envs, 'hosts': hosts})
