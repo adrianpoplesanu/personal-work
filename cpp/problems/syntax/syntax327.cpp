1cbf583add0ce8964db87bf5bc272b4943d5874e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-07-30 23:24:09 +0300

    cppcon lambdas series - added url

--- cpp/problems/syntax/syntax327.cpp
+++ cpp/problems/syntax/syntax327.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // cppcon lambdas series
+// https://www.youtube.com/watch?v=3jCOwajNch0&list=RDQM0eYw2efAwBQ&start_radio=1&ab_channel=CppCon
 
 int plus1(int x) {
     return x + 1;
