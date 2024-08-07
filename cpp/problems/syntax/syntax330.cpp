55c14e117b832c7c57e72fef29e10a34add4a885
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-08-07 22:08:19 +0300

    cppcon lambdas series - unsing template and typedef

---
+++ cpp/problems/syntax/syntax330.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<typename T>
+T plus1(T x) {
+    return x + 1;
+}
+
+int main(int argc, char *argv[]) {
+    auto a = plus1(5);
+    std::cout << a << "\n";
+
+    auto b = plus1(1.4);
+    std::cout << b << "\n";
+    return 0;
+}
