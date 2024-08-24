698898ffa8a2fba525af5b771e09db0017ec1b6b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-08-24 14:52:26 +0300

    assert in cpp

---
+++ cpp/problems/syntax/syntax333.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    assert(1 == 1);
+    return 0;
+}
---
+++ cpp/problems/syntax/syntax334.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    assert(1 == 2);
+    return 0;
+}
