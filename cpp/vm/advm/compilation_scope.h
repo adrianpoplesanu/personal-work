539358c346165bcb25915cc1d39ad0de0b9bf104
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-10-20 00:52:27 +0300

    compilation scope placeholder

---
+++ cpp/vm/advm/compilation_scope.h
@@ -0,0 +1,14 @@
+#ifndef __COMPILATION_SCOPE_H
+#define __COMPILATION_SCOPE_H
+
+#include "instructions.h"
+#include "emitted_instruction.h"
+
+class CompilationScope {
+public:
+    Instructions instructions; // TODO: check if this is correct
+    EmittedInstruction previousInstruction = EmittedInstruction();
+    EmittedInstruction lastInstruction = EmittedInstruction();
+};
+
+#endif
