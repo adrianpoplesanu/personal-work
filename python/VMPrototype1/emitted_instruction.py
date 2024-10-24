7879703dcae85ca4ce971a9394660b9d6095788e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-10-25 01:38:50 +0300

    fixed typo

--- python/VMPrototype1/emitted_instruction.py
+++ python/VMPrototype1/emitted_instruction.py
@@ -1,4 +1,4 @@
 class EmittedInstruction:
-    def __init__(self, opcode=None, position=Noneq):
+    def __init__(self, opcode=None, position=None):
         self.opcode = opcode
         self.position = position
