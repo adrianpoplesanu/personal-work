b77b96ec59e78618120ddc2c8667f3cbc2c6c1cb
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-11-23 03:53:16 +0200

    added local scope

--- python/VMPrototype1/symbol_table.py
+++ python/VMPrototype1/symbol_table.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 
 class SymbolScope:
@@ -7,6 +7,7 @@ class SymbolScope:
 
 
 GlobalScope = SymbolScope("GLOBAL")
+LocalScope = SymbolScope("LOCAL")
 
 
 class Symbol:
@@ -18,20 +19,33 @@ class Symbol:
 
 class SymbolTable:
     def __init__(self, store: Dict[str, Symbol] = None, num_definitions: int = None):
+        self.outer: Optional[SymbolTable] = None
         self.store = store
         self.num_definitions = num_definitions
 
     def define(self, name: str):
-        symbol = Symbol(name=name, index=self.num_definitions, scope=GlobalScope)
+        symbol = Symbol(name=name, index=self.num_definitions)
+        if self.outer is None:
+            symbol.scope = GlobalScope
+        else:
+            symbol.scope = LocalScope
         self.store[name] = symbol
         self.num_definitions += 1
         return symbol
 
     def resolve(self, name: str):
-        obj = self.store[name]
+        obj = self.store.get(name)
+        if obj is None and self.outer:
+            obj = self.outer.resolve(name)
         return obj
 
 
 def new_symbol_table():
     store: Dict[str, Symbol] = {}
     return SymbolTable(store=store, num_definitions=0)
+
+
+def new_enclosed_symbol_table(outer: SymbolTable) -> SymbolTable:
+    s = new_symbol_table()
+    s.outer = outer
+    return s
