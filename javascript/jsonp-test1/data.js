1596f6e7de51621506ba6c635e70cb4b28bc581b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-06 13:06:08 +0300

    local testing jsonp

---
+++ javascript/jsonp-test1/data.js
@@ -0,0 +1,7 @@
+// asta e raspunsul serverului
+// in raspunsul serverului, se face referinta la callback-ul din client handleData care va fi rulat peste json-ul cu date
+
+handleData({
+  name: "Alice",
+  age: 30
+});
