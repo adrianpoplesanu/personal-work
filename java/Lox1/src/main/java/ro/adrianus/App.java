40ec12765b78a10b3e79cf4144c0c2322c9a39c8
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-28 19:29:29 +0300

    lox initial commit

---
+++ java/Lox1/src/main/java/ro/adrianus/App.java
@@ -0,0 +1,13 @@
+package ro.adrianus;
+
+import java.io.IOException;
+
+/**
+ * Lox implementation
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+        //System.out.println("Lox programming language");
+        Lox.run(args);
+    }
+}
---
+++ java/Lox1/src/test/java/ro/adrianus/AppTest.java
@@ -0,0 +1,19 @@
+package ro.adrianus;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
