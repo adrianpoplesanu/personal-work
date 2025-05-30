e1dda916110b26352f826332a25984d313bff1b3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-30 23:41:43 +0300

    updated CMakeLists.txt file and run a simple chunk in main

--- cpp/lox2/main.c
+++ cpp/lox2/main.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char* argv[]) {
-    printf("lox vm implementation");
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
     return 0;
 }
