26a2e0ede493299f7b8158b6a5ef47b62429f8e3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-31 11:34:32 +0300

    added debug dissable with chunk traversing

---
+++ cpp/lox2/debug.h
@@ -0,0 +1,9 @@
+#ifndef LOX2_DEBUG_H
+#define LOX2_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
