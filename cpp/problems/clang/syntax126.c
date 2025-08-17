0e8fa90d667befed281fe40bbdcd3a79834ab49e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-08-17 23:30:45 +0300

    placeholder next program

---
+++ cpp/problems/clang/syntax126.c
@@ -0,0 +1,11 @@
+// COMPILE:     gcc syntax126.c
+// USAGE:       ./a.out
+// BOOKMARK:    pg ...
+// DESCRIPTION: ...
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("running...\n");
+    return 0;
+}
