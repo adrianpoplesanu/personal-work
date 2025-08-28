623ec6c48c66f6838b37c36f04b48ba2b461f2cd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-08-29 01:24:11 +0300

    usr resilience structure

--- kotlin/BookConcurencyInKotlin04/src/main/kotlin/syntax_fun/StructureTesting.kt
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/syntax_fun/StructureTesting.kt
@@ -2,34 +2,44 @@
 
 package syntax_fun
 
-class UsrResiliencyFeatures {
+class UsrResiliencyFeatures(name: String, threshold: Int) {
     companion object {
         fun builder(): UsrResiliencyFeatures {
-            return UsrResiliencyFeatures()
+            return UsrResiliencyFeatures("undefined", 1)
         }
     }
 
+    var name: String = name
+    var threshold: Int = threshold
+
     fun build(): UsrResiliencyFeatures {
-        return UsrResiliencyFeatures()
+        return UsrResiliencyFeatures("undefined", 1)
+    }
+
+    fun build(name: String, threshold: Int): UsrResiliencyFeatures {
+        return UsrResiliencyFeatures(name, threshold)
     }
 }
 
 fun <T> UsrResiliencyFeatures.circuitBreaker(block: () -> T): T {
+    println(name)
+    println(threshold)
     return block()
 }
 
 class UsrResiliencyFeaturesDsl(name: String) {
     private val builder = UsrResiliencyFeatures.builder()
     var threshold: Int = 0
+    var name = name
 
-    internal fun build() = builder.build()
+    internal fun build() = builder.build(name, threshold)
 }
 
 fun resiliency2(name: String, dsl: UsrResiliencyFeaturesDsl.() -> Unit): UsrResiliencyFeatures =
     UsrResiliencyFeaturesDsl(name).also(dsl).build()
 
 class UcpService {
-    private val resiliency = resiliency2("acp-service") {
+    private val resiliency = resiliency2("ucp-service") {
         threshold = 10
     }
 
