49a8b6aee0e1b7ed091aca56d228545554db3404
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-09-02 22:44:27 +0300

    a correct implementation of batch sizing

--- kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/Palindrome5.kt
+++ kotlin/BookConcurencyInKotlin04/src/main/kotlin/ch01/Palindrome5.kt
@@ -3,7 +3,7 @@ package ch01
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
-class Palindrome4 {
+class Palindrome5 {
 
     private fun isPalindrome(word : String): Boolean {
         return word == word.reversed()
@@ -13,19 +13,20 @@ class Palindrome4 {
         return words.filter { word -> isPalindrome(word) }
     }
 
-    suspend fun getAllPalindromes(n : Int): List<String> {
+    suspend fun getAllPalindromes(batchSize : Int): List<String> {
         var words = readWordsFromJson("/words2.json")
         var result : List<String> = mutableListOf()
 
-        val batchSize = words.size / n
+        val steps = words.size / batchSize + if (words.size % batchSize != 0) 1 else 0
         var partialResults = mutableListOf<Deferred<List<String>>>()
 
-        for (i in 0 until n) {
-            var partialResult = GlobalScope.async { filterPalindromes(words.subList(i * batchSize, (i + 1) * batchSize - 1)) }
+        for (i in 0 until steps) {
+            println("step ${i}: ${words.subList(i * batchSize, Math.min((i + 1) * batchSize, words.size))}")
+            var partialResult = GlobalScope.async { filterPalindromes(words.subList(i * batchSize, Math.min((i + 1) * batchSize, words.size))) }
             partialResults.add(partialResult)
         }
 
-        for (i in 0 until n) {
+        for (i in 0 until steps) {
             val partial = partialResults[i].await()
             result += partial
         }
@@ -40,8 +41,8 @@ class Palindrome4 {
         @JvmStatic
         fun main(args: Array<String>) = runBlocking {
             val time = measureTimeMillis {
-                var palindrome4 = Palindrome4()
-                val palindromes = palindrome4.getAllPalindromes(1) // if n = 5, should be 2 lists on 5 and 3 elements
+                var palindrome5 = Palindrome5()
+                val palindromes = palindrome5.getAllPalindromes(5)
                 println(palindromes)
             }
             println ("took $time miliseconds")
