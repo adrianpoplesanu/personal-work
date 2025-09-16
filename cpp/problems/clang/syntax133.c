a83d26c4ca1564b712acadb7eda0990b7dc9996b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-09-16 21:53:16 +0300

    placeholder for dcl exercise

---
+++ cpp/problems/clang/syntax133.c
@@ -0,0 +1,12 @@
+// COMPILE:     gcc syntax133.c
+// USAGE:       ./a.out
+// BOOKMARK:    pg 110
+// DESCRIPTION: dcl
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("running...\n");
+    return 0;
+}
+
