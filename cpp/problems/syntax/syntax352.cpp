3c105bf07b43db1ef0560eff1818dc43d300dc8f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-12-17 23:25:28 +0200

    funny way of shuffling program output

---
+++ cpp/problems/syntax/syntax352.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    //  ./syntax352 | sort -R
+    for (int i = 0; i < 100; i++) {
+        std::cout << i << "\n";
+    }
+    return 0;
+}
