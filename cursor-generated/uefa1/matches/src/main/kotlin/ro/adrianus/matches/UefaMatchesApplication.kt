f82db0fc4a3bf813d8a8342de8b8adb066f56247
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2026-02-20 07:43:35 +0200

    playing around with intellij and cursor

---
+++ cursor-generated/uefa1/matches/src/main/kotlin/ro/adrianus/matches/UefaMatchesApplication.kt
@@ -0,0 +1,11 @@
+package ro.adrianus.matches
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UefaMatchesApplication
+
+fun main(args: Array<String>) {
+	runApplication<UefaMatchesApplication>(*args)
+}
---
+++ cursor-generated/uefa1/matches/src/test/kotlin/ro/adrianus/matches/UefaMatchesApplicationTests.kt
@@ -0,0 +1,13 @@
+package ro.adrianus.matches
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UefaMatchesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
