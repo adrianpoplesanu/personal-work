21226a47605cc1c06b2765bedc3bc49e4ff8441e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2026-08-23 01:41:12 +0300

    simple pointer to an array

---
+++ academic1/ts2/program005.cpp
@@ -0,0 +1,33 @@
+/*
+   exercise: 005
+   page: 12
+   description: pointing to an array
+   command: ./program005
+*/
+
+#include <iostream>
+#include <chrono>
+
+#define PRINT_EXECUTION_DURATION 0
+
+int main(int argc, char *argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //... start code here
+
+    float a[10], *b;
+    a[7] = 5;
+    b = a;
+    std::cout << b[7] << "\n";
+
+    //... end code here
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    if (PRINT_EXECUTION_DURATION) {
+        std::cout << "Execution time: " << duration.count() << " ms\n";
+    }
+    return 0;
+}
+
