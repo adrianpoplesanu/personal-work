169e67e44042c1df56177b0e40330a9fda17bda7
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2026-09-06 22:54:04 +0300

    stacks and queues

---
+++ java/Goodrich3/src/chapter6/ArrayStack.java
@@ -0,0 +1,61 @@
+package chapter6;
+
+public class ArrayStack<E> implements Stack<E> {
+    public static final int CAPACITY = 1000;
+    private E[] data;
+    private int t = -1;
+
+    public ArrayStack() {
+        this(CAPACITY);
+    }
+
+    public ArrayStack(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public int size() {
+        return t + 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return t == -1;
+    }
+
+    @Override
+    public void push(E e) {
+        if (size() == data.length) {
+            throw new IllegalStateException("Stack is full");
+        }
+        data[++t] = e;
+    }
+
+    @Override
+    public E top() {
+        if (isEmpty()) return null;
+        return data[t];
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) return null;
+        E answer = data[t];
+        data[t] = null;
+        t--;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        System.out.println(stack.isEmpty());
+        stack.push(11);
+        stack.push(12);
+        stack.push(13);
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.top());
+        while(!stack.isEmpty()) {
+            System.out.print(stack.pop()+ " ");
+        }
+    }
+}
