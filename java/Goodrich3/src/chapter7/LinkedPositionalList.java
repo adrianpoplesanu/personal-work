9374af6d579745e6a3301311a369bf0af033409d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2026-09-17 11:18:36 +0300

    linked positional list implementation

--- java/Goodrich3/src/chapter7/LinkedPositionalList.java
+++ java/Goodrich3/src/chapter7/LinkedPositionalList.java
@@ -40,63 +40,147 @@ public class LinkedPositionalList<E> implements PositionalList<E> {
         }
     }
 
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public LinkedPositionalList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid p");
+        Node<E> node = (Node<E>) p;
+        if (node.getNext() == null) {
+            throw new IllegalArgumentException("p is no longer in the list");
+        }
+        return node;
+    }
+
+    private Position<E> position(Node<E> node) {
+        if (node == header || node == trailer) {
+            return null;
+        }
+        return node;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Position<E> first() {
-        return null;
+        return position(header.getNext());
     }
 
     @Override
     public Position<E> last() {
-        return null;
+        return position(trailer.getPrev());
     }
 
     @Override
     public Position<E> before(Position<E> p) throws IllegalStateException {
-        return null;
+        Node<E> node = validate(p);
+        return position(node.getPrev());
     }
 
     @Override
     public Position<E> after(Position<E> p) throws IllegalStateException {
-        return null;
+        Node<E> node = validate(p);
+        return position(node.getNext());
+    }
+
+    private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
+        Node<E> newest = new Node<>(e, pred, succ);
+        pred.setNext(newest);
+        succ.setPrev(newest);
+        size++;
+        return newest;
     }
 
     @Override
     public Position<E> addFirst(E e) {
-        return null;
+        return addBetween(e, header, header.getNext());
     }
 
     @Override
     public Position<E> addLast(E e) {
-        return null;
+        return addBetween(e, trailer.getPrev(), trailer);
     }
 
     @Override
-    public Position<E> addBefore(E e) throws IllegalArgumentException {
-        return null;
+    public Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return addBetween(e, node.getPrev(), node);
     }
 
     @Override
-    public Position<E> addAfter(E e) throws IllegalArgumentException {
-        return null;
+    public Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return addBetween(e, node, node.getNext());
     }
 
     @Override
     public E set(Position<E> p, E e) throws IllegalArgumentException {
-        return null;
+        Node<E> node = validate(p);
+        E answer = node.getElement();
+        node.setElement(e);
+        return answer;
     }
 
     @Override
     public E remove(Position<E> p) throws IllegalArgumentException {
-        return null;
+        Node<E> node = validate(p);
+        Node<E> predecessor = node.getPrev();
+        Node<E> successor = node.getNext();
+        predecessor.setNext(successor);
+        successor.setPrev(predecessor);
+        size--;
+        E answer = node.getElement();
+        node.setElement(null);
+        node.setPrev(null);
+        node.setNext(null);
+        return answer;
+    }
+
+    public void walk() {
+        Node<E> current = header.getNext();
+        while(current != trailer) {
+            System.out.print(current.getElement() + " ");
+            current = current.getNext();
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedPositionalList<Integer> list = new LinkedPositionalList<>();
+        list.addFirst(11);
+        list.addLast(12);
+        list.addLast(45);
+        list.addFirst(5);
+
+        Position<Integer> first = list.first();
+        System.out.println(first.getElement());
+
+        Position<Integer> last = list.last();
+        System.out.println(last.getElement());
+
+        System.out.print("[ WALK ] ");
+        list.walk();
+        System.out.println();
+
+        System.out.print("[ RESULT ] ");
+        while(!list.isEmpty()) {
+            Position<Integer> current = list.first();
+            System.out.print(current.getElement() + " ");
+            list.remove(current);
+        }
     }
 }
--- java/Goodrich3/src/chapter7/PositionalList.java
+++ java/Goodrich3/src/chapter7/PositionalList.java
@@ -9,8 +9,8 @@ public interface PositionalList<E> {
     Position<E> after(Position<E> p) throws IllegalStateException;
     Position<E> addFirst(E e);
     Position<E> addLast(E e);
-    Position<E> addBefore(E e) throws IllegalArgumentException;
-    Position<E> addAfter(E e) throws IllegalArgumentException;
+    Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException;
+    Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException;
     E set(Position<E> p, E e) throws IllegalArgumentException;
     E remove(Position<E> p) throws IllegalArgumentException;
 }
