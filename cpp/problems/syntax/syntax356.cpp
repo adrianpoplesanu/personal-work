d8758a08d8fde1482f2f3c986a1e5237066c82c3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2026-09-23 23:44:11 +0300

    dp style walking a matrix with index start=1

---
+++ cpp/problems/syntax/syntax356.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+void init(int a[50][50], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = 0;
+        }
+    }
+}
+
+void print(int a[50][50], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int a[50][50];
+    int n = 10;
+    int step = 1;
+
+    init(a, 50);
+
+    for (int i = 1; i <= n; i++) {
+        a[i][i] = step;
+    }
+
+    for (int l = 2; l <= n; l++) {
+        step++;
+        for (int i = 1; i <= n - l + 1; i++) {
+            int j = i + l - 1;
+            a[i][j] = step;
+        }
+    }
+
+    print(a, 50);
+
+    return 0;
+}
